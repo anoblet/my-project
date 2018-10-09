3d9bf37b15a5279169d43cd29629b1fff790ec36
Andrew Noblet <andrewbnoblet@gmail.com>
2018-10-08 22:50:40 -0400

    Chore: Add files

---
+++ src/my-app/my-app.js
@@ -0,0 +1,12 @@
+import { BaseElement } from '../BaseElement/BaseElement.js';
+
+class MyApp extends BaseElement {
+  static get properties() {
+  }
+
+  render() {
+    test
+  }
+}
+
+window.customElements.define('my-app', MyApp);
