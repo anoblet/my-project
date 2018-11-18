48ad6ee16b6d3e8785cae303258538b71e3ed8b5
Andrew Noblet <andrewbnoblet@gmail.com>
2018-11-18 16:07:09 -0500

    Chore: Save

--- src/actions/Settings.js
+++ src/actions/Settings.js
@@ -1,39 +1,16 @@
 export const CHANGE_THEME = 'CHANGE_THEME';
 export const TOGGLE_DEBUG = 'TOGGLE_DEBUG';
 
-// export const changeTheme = () => (dispatch, getState) => {
-//   const state = getState();
-//   const theme = state.settings.theme == 'light' ? 'dark' : 'light';
-//   dispatch({
-//     type: CHANGE_THEME,
-//     theme,
-//   });
-// };
-
 export const setDebug = (status) => (dispatch) => {
   dispatch({
-    type: TOGGLE_DEBUG,
-    status
+    status,
+    type: TOGGLE_DEBUG
   });
 }
-
-// export const setDebug = (status) => {
-//   return {
-//     type: TOGGLE_DEBUG,
-//     status
-//   };
-// };
 
 export const setTheme = (theme) => (dispatch) => {
   dispatch({
-    type: CHANGE_THEME,
-    theme
+    theme,
+    type: CHANGE_THEME
   });
 }
-
-// export const setTheme = (theme) => {
-//   return {
-//     type: CHANGE_THEME,
-//     theme
-//   };
-// };
