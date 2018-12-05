68230c2aec85feb847dfb601d2e29150bfd3a3de
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-05 08:41:44 -0500

    Chore: Save

--- src/components/PageHome/PageHomeTemplate.ts
+++ src/components/PageHome/PageHomeTemplate.ts
@@ -11,28 +11,36 @@ export default function ({ user }: any) {
     <my-grid id="content-grid" style="grid-template-columns: repeat(auto-fit, minmax(350px, 1fr) );">
       <my-card style="grid-column: 1/-1">
         <div slot="title">Welcome</div>
-        Welcome ${user.name ? user.name : 'Guest'}! ${!user.signedIn ? html`Sign in to save settings` : html`You are currently signed in: Your settings will now be saved`}.
+        <div slot="content">
+          Welcome ${user.name ? user.name : 'Guest'}! ${!user.signedIn ? html`Sign in to save settings` : html`You are currently signed in: Your settings will now be saved`}.
+        </div>
       </my-card>
       <my-card>
         <div slot="title">State</div>
+        <div slot="content">
           <pre style="overflow: hidden;">${JSON.stringify(this.state, null, 2)};</pre>
+        </div>
       </my-card>
       <my-card>
         <div slot="title">Firebase</div>
-        ${user.signedIn ? html`
-          ${until(
-            this.getDocument('theme').then((document: any) => {
-              return html`
-                <pre>${JSON.stringify(document, null, 2)}</pre>
-              `
-            }),
-            html`<my-loader></my-loader>`
-          )}
-        ` : html`Sign in to see a Firebase document`}
+        <div slot="content">
+          ${user.signedIn ? html`
+            ${until(
+              this.getDocument('theme').then((document: any) => {
+                return html`
+                  <pre>${JSON.stringify(document, null, 2)}</pre>
+                `
+              }),
+              html`<my-loader></my-loader>`
+            )}
+          ` : html`Sign in to see a Firebase document`}
+        </div>
       </my-card>
       <my-card style="grid-column: 1/-1">
         <div slot="title">Lorem Ipsum</div>
-        <lorem-ipsum count="100"></lorem-ipsum>
+        <div slot="content">
+          <lorem-ipsum count="100"></lorem-ipsum>
+        </div>
       </my-card>
     </my-grid>
   `
