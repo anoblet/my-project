af0f2184c3fbde5606a247fbc4e2facd5bfc5989
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-13 19:44:10 -0500

    Chore: Save

--- packages/DrawerAbsolute/DrawerAbsolute.ts
+++ packages/DrawerAbsolute/DrawerAbsolute.ts
@@ -1,17 +1,23 @@
 import { html, LitElement, property } from '@polymer/lit-element';
-import { connect } from 'pwa-helpers/connect-mixin.js';
-import { Mixin } from '../../../packages/Mixin';
-import { StateMixin } from '../../../packages/StateMixin';
-import { TaskMixin } from '../../../packages/TaskMixin';
-import { store } from '../../store.js';
+import { Mixin } from '../Mixin';
+import { StateMixin } from '../StateMixin';
+import { TaskMixin } from '../TaskMixin';
 import * as style from './DrawerAbsolute.scss';
 import Template from './DrawerAbsoluteTemplate';
 
-export class DrawerAbsolute extends Mixin(connect(store)(LitElement), [StateMixin, TaskMixin]) {
+export class DrawerAbsolute extends Mixin(LitElement, [StateMixin, TaskMixin]) {
   public render() {
     return html`
-      <style>${style}</style>
-      ${!this.taskPending ? Template.bind(this)(this.state) : html`<my-loader></my-loader>`}
+      <style>
+        ${style}
+      </style>
+      ${
+        !this.taskPending
+          ? Template.bind(this)(this.state)
+          : html`
+              <my-loader></my-loader>
+            `
+      }
     `;
   }
 }
--- packages/DrawerAbsolute/DrawerAbsoluteTemplate.ts
+++ packages/DrawerAbsolute/DrawerAbsoluteTemplate.ts
@@ -1,24 +1,6 @@
 import { html } from '@polymer/lit-element';
-export default function({ theme, user }: any) {
+export default function() {
   return html`
-  <my-card collapsible grow full-height>
-    <h3 slot="title">Menu</h3>
-    <div slot="content">
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/user">User</a>
-        </li>
-        <li>
-          <a href="/theme">Theme</a>
-        </li>
-        <li>
-          <a href="/info">Info</a>
-        </li>
-      </ul>
-    </div>
-  </my-card>
+    <slot></slot>
   `;
 }
