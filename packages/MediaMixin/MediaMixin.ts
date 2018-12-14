7faea4e5d5c4241fe5140f5177c32ef3289a4325
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-14 09:28:29 -0500

    Chore: Save

---
+++ packages/MediaMixin/MediaMixin.ts
@@ -0,0 +1,25 @@
+import { html, property } from '@polymer/lit-element';
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+export function MediaMixin<B extends Constructor<HTMLElement>>(baseClass: B) {
+  class MixinClass extends baseClass {
+    @property({ type: String }) public mediaSize: string;
+
+    constructor(...args: any[]) {
+      super(...args);
+      const observer = window.matchMedia('(max-width: 500px)');
+      const myListener = (media: any) => {
+        if (media.matches) {
+          this.mediaSize = 'small';
+        } else {
+          this.mediaSize = undefined;
+        }
+      };
+      observer.addListener(myListener);
+      myListener(observer);
+    }
+  }
+
+  return MixinClass;
+}
