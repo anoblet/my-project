9587efafc3cd7fef20ae341a340598b9a93d8521
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-16 15:53:41 -0500

    Feat: Add column option to grid

--- packages/my-grid/MyGrid.ts
+++ packages/my-grid/MyGrid.ts
@@ -1,17 +1,24 @@
-import { LitElement, property } from '@polymer/lit-element';
-import { BaseMixin } from '@anoblet/base-mixin';
+import { LitElement, property } from "@polymer/lit-element";
+import { BaseMixin } from "@anoblet/base-mixin";
 
 // import './src/components/MyRow/MyRow'
 // import './src/components/MyColumn/MyColumn'
 
-import Template from './View/MyGrid';
+import Template from "./View/MyGrid";
 
 export class MyGrid extends BaseMixin(LitElement) {
-  @property({type: String}) direction = 'row'
-  
+  @property({ type: String }) direction = "row";
+  @property({ type: Number }) columns: number;
+
+  connectedCallback() {
+    super.connectedCallback();
+    if (this.columns)
+      this.style.gridTemplateColumns = `repeat(${this.columns}, 1fr)`;
+  }
+
   render() {
     return Template.bind(this)();
   }
 }
 
-window.customElements.define('my-grid', MyGrid);
+window.customElements.define("my-grid", MyGrid);
