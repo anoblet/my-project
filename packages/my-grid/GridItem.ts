3d7b44dc45ade9307c3cb7bde8efd077b2d80762
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-17 21:42:21 -0500

    Chore: Save

---
+++ packages/my-grid/GridItem.ts
@@ -0,0 +1,23 @@
+import { html, LitElement, property } from "@polymer/lit-element";
+
+import * as style from "./GridItem.scss";
+
+export class GridItem extends LitElement {
+  @property({ type: Boolean, reflect: true }) center: boolean;
+  @property({ type: Number }) span: number;
+
+  connectedCallback() {
+    super.connectedCallback();
+    if (this.span) this.style.gridColumn = `span ${this.span}`;
+  }
+
+  render() {
+    return html`
+      <style>
+        ${style}</style
+      ><slot></slot>
+    `;
+  }
+}
+
+window.customElements.define("grid-item", GridItem);
