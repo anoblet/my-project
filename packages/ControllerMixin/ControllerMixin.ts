bbb06811243397ebb98bddbd53ee9dbe5b162dd7
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-19 08:54:23 -0500

    Chore: Save

---
+++ packages/ControllerMixin/ControllerMixin.ts
@@ -0,0 +1,11 @@
+import { html, property } from "@polymer/lit-element";
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+export function ControllerMixin<B extends Constructor<HTMLElement>>(
+  baseClass: B
+) {
+  class MixinClass extends baseClass {}
+
+  return MixinClass;
+}
