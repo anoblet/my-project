fb7186a5b189c9d99e055253e043718e87969655
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 17:16:42 -0500

    Chore: Save

--- src/User/SettingsComponent.ts
+++ src/User/SettingsComponent.ts
@@ -5,10 +5,7 @@ import { LitElement, html, property } from "@polymer/lit-element";
 import { until } from "lit-html/directives/until";
 
 export class SettingsComponent extends LitElement {
-  @property({ type: String }) id: string;
-  @property({ type: String }) author: string;
-  @property({ type: String }) title: string;
-  @property({ type: String }) content: string;
+  form() {}
 
   render() {
     return html`
