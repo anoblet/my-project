e22732a174988e40bbaea8863a2f61ffc0da1925
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 17:49:03 -0500

    Chore: Save

--- src/components/Menu/MenuComponent.ts
+++ src/components/Menu/MenuComponent.ts
@@ -43,6 +43,7 @@ export class MenuComponent extends Mixin(connect(store)(LitElement), [
   }
 
   stateChanged(state: any) {
+    console.log(state);
     if (state.settings.rightClick) {
       document.oncontextmenu = (e: any) => this._onContextMenu(e);
     } else {
