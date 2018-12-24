54cb42270aaffc816a4f48516327b03b962c6228
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 18:02:25 -0500

    Chore: Save

--- src/components/MyApp/MyAppTemplate.ts
+++ src/components/MyApp/MyAppTemplate.ts
@@ -38,7 +38,6 @@ export default function({ router, user }: any) {
                     user.signedIn
                       ? html`
                           <li><a href="/theme">Theme</a></li>
-
                           <li><a href="/user-settings">Settings</a></li>
                         `
                       : ""
