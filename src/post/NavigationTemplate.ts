923f19dd0f6d0b05c1cbc01bf3ca30e9fccff80b
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-23 19:29:50 -0500

    Chore: Save

--- src/post/NavigationTemplate.ts
+++ src/post/NavigationTemplate.ts
@@ -3,7 +3,6 @@ import { html } from "@polymer/lit-element";
 export default function() {
   return html`
     <a href="/post/create"><mwc-button outlined>New</mwc-button></a>
-
     <a href="/post/read"><mwc-button outlined>List</mwc-button></a>
     <!--
       <a href="/post/edit"><mwc-button outlined>Update</mwc-button></a>
