a0841c43d377c273b63b2dc6b83f681702d51360
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 17:38:53 -0500

    Chore: Save

--- src/User/SettingsComponent.ts
+++ src/User/SettingsComponent.ts
@@ -5,7 +5,12 @@ import { LitElement, html, property } from "@polymer/lit-element";
 import { settings } from "./Settings";
 import { until } from "lit-html/directives/until";
 
-export class SettingsComponent extends LitElement {
+export class SettingsComponent extends Mixin(connect(store)(LitElement), [
+  BaseMixin,
+  TaskMixin,
+  StateMixin,
+  FirebaseMixin
+]) {
   form() {
     return html`
       <form>
