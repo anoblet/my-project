5889fbcafc1f9904cafe631a3486f5d64adf1543
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 09:50:07 -0500

    Chore: Save

--- src/post/PostController.ts
+++ src/post/PostController.ts
@@ -53,7 +53,9 @@ export class PostController extends Mixin(LitElement, [
 
   create() {
     const post = new Post();
-    this._template = import("./PostCreateTemplate").then((module) module.default.bind(this)(this))
+    this._template = import("./PostCreateTemplate").then(module =>
+      module.default.bind(this)(this)
+    );
   }
 
   edit() {
