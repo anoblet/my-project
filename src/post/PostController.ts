735611bcb966a21ecd87f074b5391a510482295c
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 09:49:47 -0500

    Chore: Save

--- src/post/PostController.ts
+++ src/post/PostController.ts
@@ -53,63 +53,7 @@ export class PostController extends Mixin(LitElement, [
 
   create() {
     const post = new Post();
-    this._template = html`
-      <form>
-        <vaadin-form-layout>
-        ${post.fields.map(
-          (field: any) =>
-            html`
-              ${
-                field.type == "text"
-                  ? html`
-                      <vaadin-text-field
-                        name="${field.name}"
-                        label="${field.label}"
-                      ></vaadin-text-field>
-                    `
-                  : ""
-              }
-              ${
-                field.type == "textarea"
-                  ? html`
-                      <vaadin-text-area
-                        colspan="2"
-                        name="${field.name}"
-                        label="${field.label}"
-                      ></vaadin-text-area>
-                    `
-                  : ""
-              }
-            `
-        )}
-        <vaadin-form-layout>
-        <button
-          @click="${(e: Event) => {
-            e.preventDefault();
-            const data: any = {};
-            data.title = this.shadowRoot.querySelector("[name='title']").value;
-            data.author = this.shadowRoot.querySelector(
-              "[name='author']"
-            ).value;
-            data.content = this.shadowRoot.querySelector(
-              "[name='content']"
-            ).value;
-            this.addDocument({ path: "posts", data }).then((result: any) => {
-              this.shadowRoot.querySelector(
-                "#result"
-              ).innerHTML = `Document created: ${result}. Wait 3 seconds for a redirec to your post.`;
-              setTimeout(
-                () => store.dispatch(navigate(`/post/read/${result}`)),
-                3000
-              );
-            });
-          }}"
-        >
-          Submit
-        </button>
-      </form>
-      <div id="result"></div>
-    `;
+    this._template = import("./PostCreateTemplate").then((module) module.default.bind(this)(this))
   }
 
   edit() {
