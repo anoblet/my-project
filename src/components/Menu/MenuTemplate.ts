d7765f064f80a927db782071131e515a6eb9c857
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 18:50:40 -0500

    Chore: Save

--- src/components/Menu/MenuTemplate.ts
+++ src/components/Menu/MenuTemplate.ts
@@ -4,14 +4,7 @@ import { html } from "@polymer/lit-element";
 export default function() {
   return html`
     <ul id="menu">
-      <mwc-fab icon="home"></mwc-fab>
-      <mwc-fab icon="home"></mwc-fab>
-      <mwc-fab icon="home"></mwc-fab>
-      <mwc-fab icon="home"></mwc-fab>
-      <mwc-fab icon="home"></mwc-fab>
-      <mwc-fab icon="home"></mwc-fab>
-      <mwc-fab icon="home"></mwc-fab>
-      <mwc-fab icon="home"></mwc-fab>
+      <div><mwc-fab icon="home"></mwc-fab></div>
     </ul>
   `;
 }
