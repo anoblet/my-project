873ac6afedc3e968e19e01b1c30163262e8c00af
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 12:34:23 -0500

    Chore: Save

--- src/post/PostCreateTemplate.ts
+++ src/post/PostCreateTemplate.ts
@@ -39,7 +39,7 @@ export default function() {
               )
             }
           </vaadin-form-layout>
-          <div style="display: flex; justify-content: end;">
+          <div style="display: flex; justify-content: flex-end;">
             <mwc-button @click="${(e: Event) => this.submitForm(e)}"
               >Submit</mwc-button
             >
