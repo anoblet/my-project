cfeb302b2df2e47ea7514029cd4c70e232021d3f
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 14:00:30 -0500

    Chore: Save

--- src/components/Menu/MenuComponent.ts
+++ src/components/Menu/MenuComponent.ts
@@ -27,7 +27,7 @@ export class MenuComponent extends Mixin(LitElement, [BaseMixin]) {
     super.disconnectedCallback();
     // window.removeEventListener("hashchange", this._boundListener);
   }
-  _onKeyUp(e: any) {
+  _onContextMenu(e: any) {
     // if (e.shiftKey && e.keyCode == 32) {
     //   this.hidden = !this.hidden;
     // }
