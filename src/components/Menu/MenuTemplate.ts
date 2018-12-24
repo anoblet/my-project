e25725b7cc9c35d2e9d5bbf1e3df74eb9c5ae659
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 18:51:57 -0500

    Chore: Save

--- src/components/Menu/MenuTemplate.ts
+++ src/components/Menu/MenuTemplate.ts
@@ -4,14 +4,14 @@ import { html } from "@polymer/lit-element";
 export default function() {
   return html`
     <ul id="menu">
-      <div><mwc-fab icon="home"></mwc-fab></div>
-      <div><mwc-fab icon="home"></mwc-fab></div>
-      <div><mwc-fab icon="home"></mwc-fab></div>
-      <div><mwc-fab icon="home"></mwc-fab></div>
-      <div><mwc-fab icon="home"></mwc-fab></div>
-      <div><mwc-fab icon="home"></mwc-fab></div>
-      <div><mwc-fab icon="home"></mwc-fab></div>
-      <div><mwc-fab icon="home"></mwc-fab></div>
+      <li><mwc-fab icon="home"></mwc-fab></li>
+      <li><mwc-fab icon="home"></mwc-fab></li>
+      <li><mwc-fab icon="home"></mwc-fab></li>
+      <li><mwc-fab icon="home"></mwc-fab></li>
+      <li><mwc-fab icon="home"></mwc-fab></li>
+      <li><mwc-fab icon="home"></mwc-fab></li>
+      <li><mwc-fab icon="home"></mwc-fab></li>
+      <li><mwc-fab icon="home"></mwc-fab></li>
     </ul>
   `;
 }
