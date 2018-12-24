7b932eece9873efd74719749bbfa53491acf1ca4
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 13:15:34 -0500

    Chore: Save

--- src/components/MyApp/MyApp.ts
+++ src/components/MyApp/MyApp.ts
@@ -19,7 +19,7 @@ import { connect } from "pwa-helpers/connect-mixin.js";
 import { connectRouter } from "lit-redux-router";
 import { runtime } from "../../Runtime";
 import { store } from "../../store.js";
-import { store } from "../../Menu";
+import "../../Menu";
 
 connectRouter(store);
 
