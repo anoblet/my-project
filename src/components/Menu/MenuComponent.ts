daede10fbdb5d2edf9bb99e7fa5ee88cb67a5523
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-24 14:01:11 -0500

    Chore: Save

--- src/components/Menu/MenuComponent.ts
+++ src/components/Menu/MenuComponent.ts
@@ -19,7 +19,7 @@ export class MenuComponent extends Mixin(LitElement, [BaseMixin]) {
 
   connectedCallback() {
     if (super.connectedCallback) super.connectedCallback();
-    // document.onkeyup = (e: any) => this._onKeyUp(e);
+    document.onkeyup = (e: any) => this._onKeyUp(e);
     window.oncontextmenu = (e: any) => this._onKeyUp(e);
   }
 
