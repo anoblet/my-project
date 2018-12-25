70ca9693f3d40aa6e45e9ca17cf6c14c23083657
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-25 03:56:16 -0500

    Chore: Save

--- src/components/Menu/MenuTemplate.ts
+++ src/components/Menu/MenuTemplate.ts
@@ -8,7 +8,9 @@ export default function() {
     </div>
     <ul id="menu">
       <li><mwc-fab icon="home"></mwc-fab></li>
-      <li><mwc-fab icon="subject"></mwc-fab></li>
+      <li>
+        <a href="/post"><mwc-fab icon="subject"></mwc-fab></a>
+      </li>
       <li><mwc-fab icon="brush"></mwc-fab></li>
       <li><mwc-fab></mwc-fab></li>
       <li><mwc-fab></mwc-fab></li>
