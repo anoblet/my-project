b8adaabed57f85cb21a1db6cdabee7243da7a416
Andrew Noblet <andrewbnoblet@gmail.com>
2018-12-25 09:40:34 -0500

    Chore: Save

--- src/components/Menu/MenuTemplate.ts
+++ src/components/Menu/MenuTemplate.ts
@@ -3,9 +3,9 @@ import { html } from "@polymer/lit-element";
 
 export default function() {
   return html`
-    <a href="/" style="position: absolute;">
-      <mwc-fab icon="home"></mwc-fab>
-    </a>
+    <div id="home" style="position: absolute;">
+      <a href="/"><mwc-fab icon="home"></mwc-fab></a>
+    </div>
     <ul id="menu">
       <li>
         <a href="/post"><mwc-fab icon="subject"></mwc-fab></a>
