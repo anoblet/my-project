645de16c39d1cbbac02c9b47b9ecb479b0a26632
Andrew Noblet <andrewbnoblet@gmail.com>
2019-01-21 16:13:58 -0500

    Chore: Save

--- src/Store.ts
+++ src/Store.ts
@@ -1,12 +1,3 @@
-/* @license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
-
 import {
   createStore,
   compose as origCompose,
@@ -15,9 +6,6 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import { lazyReducerEnhancer } from "pwa-helpers/lazy-reducer-enhancer.js";
-// import { LazyStore } from "pwa-helpers/lazy-reducer-enhancer";
-
-// import settings from './reducers/Settings.js';
 
 declare global {
   interface Window {
@@ -25,28 +13,20 @@ declare global {
   }
 }
 
-export interface State {
-  app: any;
-  user: any;
-  router: any;
-  theme: any;
-  addReducers: any;
-}
-
-export interface AnyAction {
-  app: any;
-  user: any;
-  router: any;
-  theme: any;
-}
-
 // Sets up a Chrome extension for time travel debugging.
 // See https://github.com/zalmoxisus/redux-devtools-extension for more information.
 const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || origCompose;
 export const store = createStore(
-  (state: State) => state,
+  (state: any) => state,
   compose(
     lazyReducerEnhancer(combineReducers),
     applyMiddleware(thunk)
   )
 );
+
+// export const store = createStore(
+//   state => state as Reducer<RootState, RootAction>,
+//   devCompose(
+//     lazyReducerEnhancer(combineReducers),
+//     applyMiddleware(thunk as ThunkMiddleware<RootState, RootAction>))
+// );
