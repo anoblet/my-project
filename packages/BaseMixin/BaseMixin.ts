07417f54c2a6a447665e181859b001660057f17e
Andrew Noblet <andrewbnoblet@gmail.com>
2019-01-31 19:28:33 -0500

    Chore: Save

--- packages/BaseMixin/BaseMixin.ts
+++ packages/BaseMixin/BaseMixin.ts
@@ -1,7 +1,4 @@
-import { html } from 'lit-element';
-import { until } from 'lit-html/directives/until';
 import { Mixin } from '@anoblet/mixin'
-import { DebugMixin } from '@anoblet/debug-mixin'
 import { HelperMixin } from '@anoblet/helper-mixin'
 
 export const BaseMixin = (superclass: any) => class extends Mixin(superclass, [/* DebugMixin */, HelperMixin]) {
