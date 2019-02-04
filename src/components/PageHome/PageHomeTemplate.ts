a56ce81805e814c0c8873366eb12f8ee373b0efb
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-04 08:55:48 -0500

    Chore: Save

--- src/components/PageHome/PageHomeTemplate.ts
+++ src/components/PageHome/PageHomeTemplate.ts
@@ -1,7 +1,12 @@
 import { html } from "lit-element";
-
+import { getReadme } from "../Readme";
 export default function() {
   return html`
-    <my-grid id="content-grid"> <blog-component> </blog-component> </my-grid>
+    <my-grid id="content-grid"
+      ><card-component title="Readme"
+        ><div slot="content">${getReadme()}</div></card-component
+      >
+      <blog-component> </blog-component>
+    </my-grid>
   `;
 }
---
+++ src/components/Readme.ts
@@ -0,0 +1,20 @@
+import { html } from "lit-element";
+import { until } from "lit-html/directives/until";
+import { unsafeHTML } from "lit-html/directives/unsafe-html";
+import { mdToHtml } from "../../packages/Showdown/Showdown";
+
+export const getReadme = () => {
+  return html`
+    ${until(
+      fetch(
+        "https://raw.githubusercontent.com/anoblet/my-project/master/README.md"
+      )
+        .then(response => response.text())
+        .then((document: any) => {
+          return html`
+            ${unsafeHTML(mdToHtml(document))}
+          `;
+        })
+    )}
+  `;
+};
