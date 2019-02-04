b373493761787b9d26e22974173b719e9efd71c7
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-04 08:22:51 -0500

    Chore: Save

--- packages/Showdown/Showdown.ts
+++ packages/Showdown/Showdown.ts
@@ -1,5 +1,12 @@
 const showdown = require("showdown");
 
+/**
+ * [Converter description]
+ * @return [description]
+ *
+ * Usage: ${unsafeHTML(mdToHtml("* Test"))}
+ */
+
 export const mdToHtml = (text: string) => {
   const converter = new showdown.Converter();
   return converter.makeHtml(text);
