6a2c0abd46811373207edb4061afe5ad24751aba
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-06 12:58:45 -0500

    Chore: Save

--- src/components/UserComponent/UserComponent.ts
+++ src/components/UserComponent/UserComponent.ts
@@ -1,20 +1,16 @@
-import { css, property, LitElement } from "lit-element";
-import { template } from "./UserComponentTemplate";
-import { style } from "./UserComponentStyle";
+import { LitElement, customElement } from "lit-element";
 
 import globalStyle from "../../GlobalStyle";
+import { style } from "./UserComponentStyle";
+import { template } from "./UserComponentTemplate";
 
+@customElement("user-component")
 export class UserComponent extends LitElement {
   static get styles() {
-    return [
-      globalStyle,
-      style
-    ];
+    return [globalStyle, style];
   }
 
   render() {
     return template.bind(this)();
   }
 }
-
-window.customElements.define("user-component", UserComponent);
