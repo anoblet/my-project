5d9c11272d8a30e4c0332c20adcd7e13e59200d8
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-07 12:48:28 -0500

    Chore: Save

--- src/components/Sitemap/Sitemap.ts
+++ src/components/Sitemap/Sitemap.ts
@@ -1,3 +1,18 @@
 import { html } from "lit-element";
 
-export const generateSitemap = (routes: any) => {};
+export const generateSitemap = (routes: any) => {
+  const filteredRoutes: any = [];
+  routes.map((route: any) => {
+    filteredRoutes.push(route.path);
+  });
+  return html`
+    <ul>
+      ${filteredRoutes.map(
+        (route: any) =>
+          html`
+            <li>${route}</li>
+          `
+      )}
+    </ul>
+  `;
+};
