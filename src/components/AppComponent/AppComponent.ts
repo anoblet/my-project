a835b4ec806eab0afdb3483c3dd63581a53fed98
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-09 15:15:16 -0500

    Chore: Save

--- src/components/AppComponent/AppComponent.ts
+++ src/components/AppComponent/AppComponent.ts
@@ -98,22 +98,22 @@ export class AppComponent extends Mixin(connect(store)(LitElement), [
     super.connectedCallback();
     // Let's set a default theme
     debug("Setting default theme");
-    (async () => {
-      await getAppSettings((document: any) => {
-        const app = {
-          settings: document
-        };
-        setState({ data: app, store, type: "app" });
-        const theme = documentToStyle(document.defaultTheme);
-        setTheme(theme, this);
-      });
-    })();
 
     this.runTasks([
       new Promise(async resolve => {
         debug("Run init methods");
         await initApp(this.firebaseConfig);
         await checkRedirect();
+        await (async () => {
+          await getAppSettings((document: any) => {
+            const app = {
+              settings: document
+            };
+            setState({ data: app, store, type: "app" });
+            const theme = documentToStyle(document.defaultTheme);
+            setTheme(theme, this);
+          });
+        })();
         await getUser({
           callback: async (user: any) => {
             // Client is not logged in, nor pending redirect
