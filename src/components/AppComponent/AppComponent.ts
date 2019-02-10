a9a7799620d0babd8b0f0cf910c80d53caa4fa74
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-09 22:15:37 -0500

    Chore: Save

--- src/components/AppComponent/AppComponent.ts
+++ src/components/AppComponent/AppComponent.ts
@@ -32,26 +32,7 @@ import template from "./AppComponentTemplate";
 import { themeStructure } from "../ThemeComponent/ThemeStructure";
 
 // import { customElement } from "lit-element";
-
-import(/* webpackChunkName: "MyFlex" */ "../../../packages/my-flex");
-import(/* webpackChunkName: "MyGrid" */ "../../../packages/my-grid");
-import(/* webpackChunkName: "MyLoader" */ "../../../packages/my-loader");
-import(/* webpackChunkName: "MyCard" */ "../../../packages/my-card");
-import(/* webpackChunkName: "MWCButton" */ "@material/mwc-button");
-import(/* webpackChunkName: "MWCIcon" */ "@material/mwc-icon");
-import(/* webpackChunkName: "MWCFab" */ "@material/mwc-fab");
-import(/* webpackChunkName: "AppHeader" */ "../AppHeader/AppHeader");
-import(/* webpackChunkName: "AppFooter" */ "../AppFooter/AppFooter"); // @deprecated import(/* webpackChunkName: "AppTheme" */ "../AppTheme/AppTheme"),
-import(/* webpackChunkName: "Drawer" */ "../DrawerComponent/Drawer");
-import(/* webpackChunkName: "ProfileMenu" */ "../ProfileMenu/ProfileMenu"); // @deprecated
-import(/* webpackChunkName: "AdminComponent" */ "../AdminComponent/AdminComponent");
-import(/* webpackChunkName: "Breadcrumb" */ "../BreadcrumbComponent/Breadcrumb");
-import(/* webpackChunkName: "LogComponent" */ "../LogComponent/LogComponent");
-import(/* webpackChunkName: "ThemeComponent" */ "../ThemeComponent/ThemeComponent");
-import(/* webpackChunkName: "MediaQuery" */ "../../../packages/MediaQuery");
-import(/* webpackChunkName: "MenuComponent" */ "../MenuComponent/MenuComponent");
-import(/* webpackChunkName: "MenuComponent" */ "../MenuComponent/MenuComponent");
-import(/* webpackChunkName: "ToastComponent" */ "../ToastComponent/ToastComponent");
+import(/* webpackChunkName: "MyFlex" */ "./imports");
 
 const getAppSettings = (callback: any) => {
   return new Promise(resolve =>
@@ -170,13 +151,14 @@ export class AppComponent extends Mixin(connect(store)(LitElement), [
     );
     setPortal(this.renderRoot.querySelector("#portal"));
     setRoutes(routes);
-    installRouter((location: any) =>
+    installRouter((location: any) => {
       handleNavigation({
         location,
         routes,
         portal: this.renderRoot.querySelector("#portal")
-      })
-    );
+      });
+      this.closeMenus();
+    });
   }
 
   public _closeDrawer() {
@@ -197,18 +179,6 @@ export class AppComponent extends Mixin(connect(store)(LitElement), [
     this.drawerOpened = false;
     const menu = this.shadowRoot.querySelector("#profile-menu");
     menu.close();
-  }
-
-  public updateStyles(theme: any) {
-    themeStructure.map((field: any) => {
-      if (theme[field.property]) {
-        if (!field.varName) {
-          const parts = field.property.split(/(?=[A-Z])/);
-          const property = parts.join("-");
-          this.style.setProperty(`--${property}`, theme[field.property]);
-        } else this.style.setProperty(field.varName, theme[field.property]);
-      }
-    });
   }
 
   // State
---
+++ src/components/AppComponent/imports.ts
@@ -0,0 +1,21 @@
+import(/* webpackChunkName: "MyFlex" */ "../../../packages/my-flex");
+import(/* webpackChunkName: "MyGrid" */ "../../../packages/my-grid");
+import(/* webpackChunkName: "MyLoader" */ "../../../packages/my-loader");
+import(/* webpackChunkName: "MyCard" */ "../../../packages/my-card");
+import(/* webpackChunkName: "MWCButton" */ "@material/mwc-button");
+import(/* webpackChunkName: "MWCIcon" */ "@material/mwc-icon");
+import(/* webpackChunkName: "MWCFab" */ "@material/mwc-fab");
+import(/* webpackChunkName: "AppHeader" */ "../AppHeader/AppHeader");
+import(/* webpackChunkName: "AppFooter" */ "../AppFooter/AppFooter"); // @deprecated import(/* webpackChunkName: "AppTheme" */ "../AppTheme/AppTheme"),
+import(/* webpackChunkName: "Drawer" */ "../DrawerComponent/Drawer");
+import(/* webpackChunkName: "ProfileMenu" */ "../ProfileMenu/ProfileMenu"); // @deprecated
+import(/* webpackChunkName: "AdminComponent" */ "../AdminComponent/AdminComponent");
+import(/* webpackChunkName: "Breadcrumb" */ "../BreadcrumbComponent/Breadcrumb");
+import(/* webpackChunkName: "LogComponent" */ "../LogComponent/LogComponent");
+import(/* webpackChunkName: "ThemeComponent" */ "../ThemeComponent/ThemeComponent");
+import(/* webpackChunkName: "MediaQuery" */ "../../../packages/MediaQuery");
+import(/* webpackChunkName: "MenuComponent" */ "../MenuComponent/MenuComponent");
+import(/* webpackChunkName: "MenuComponent" */ "../MenuComponent/MenuComponent");
+import(/* webpackChunkName: "ToastComponent" */ "../ToastComponent/ToastComponent");
+
+export default true;
