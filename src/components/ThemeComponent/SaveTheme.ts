09139518e8783682b6aa1444c2339c16147ced02
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-10 11:36:45 -0500

    Chore: Save

--- src/components/ThemeComponent/SaveTheme.ts
+++ src/components/ThemeComponent/SaveTheme.ts
@@ -1,15 +1,21 @@
 import { html } from "lit-element";
 import { setTheme } from "./ThemeComponent";
-import { addDocument } from "../../../packages/firebase-helpers/firebase-helpers";
+import {
+  addDocument,
+  getDocument
+} from "../../../packages/firebase-helpers/firebase-helpers";
 import { store } from "../../Store";
 import { fireToast } from "../ToastComponent/Toast";
 
-const saveTheme = (name: string, scope: any) => {
+const saveTheme = async (name: string, scope: any) => {
   const state = store.getState();
-  const theme = state.theme;
+  // const theme = state.theme;
+  const theme = await getDocument({
+    path: `users/${state.user.uid}/settings/theme`
+  });
   addDocument({
     path: `users/${state.user.uid}/settings/theme/savedThemes`,
-    data: { ...state.theme, ...{ name } }
+    data: { ...theme.currentTheme, ...{ name } }
   }).then(() => fireToast("Theme saved!"));
 };
 
@@ -18,14 +24,12 @@ export default function() {
     <input id="name" placeholder="Theme name" type="text" />
     <mwc-button
       outlined
-      @click="${
-        (e: any) => {
-          const field = (
-            this.renderRoot.querySelector("#name")
-          ) as HTMLInputElement;
-          saveTheme(field.value, this);
-        }
-      }"
+      @click="${(e: any) => {
+        const field = this.renderRoot.querySelector(
+          "#name"
+        ) as HTMLInputElement;
+        saveTheme(field.value, this);
+      }}"
     >
       Save theme
     </mwc-button>
