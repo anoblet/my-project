fab5abfaea22380776037f8d7ff021cfa92e2881
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-10 14:05:20 -0500

    Chore: Save

--- src/components/LogComponent/LogComponent.ts
+++ src/components/LogComponent/LogComponent.ts
@@ -18,8 +18,8 @@ export class Log extends LitElement {
                   `
                 : html`
                     <li>
-                      ${item.message}
-                      (+${Math.round(item.time - history[index - 1].time)}ms)
+                      ${item.message}:
+                      ${Math.round(item.time)}ms (+${Math.round(item.time - history[index - 1].time)}ms)
                     </li>
                   `}
             `
