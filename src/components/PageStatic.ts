ec4d8797bfb212404be804ee23a4c7ffaaeaf0e8
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-10 19:16:01 -0500

    Chore: Save

--- src/components/PageStatic.ts
+++ src/components/PageStatic.ts
@@ -8,7 +8,7 @@ class PageStatic extends LitElement {
   public render() {
     return html`
       <card-component title="Welcome">
-        Welcome! This is a static page. If you're looking for content go to <a href="/blog">/blog</a>, or view the <a href="/log">/log</a> to the see the timings of the app.
+        Welcome! This is a static page. If you're looking for content go to <a href="/blog">/blog</a>. To view the timings of the app, go to <a href="/log">/log</a>.
       </card-component>
     `;
   }
