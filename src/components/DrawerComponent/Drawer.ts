b43162e1f7f8bfcc17636c44ac45b2567df47cf7
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-11 10:58:37 -0500

    Chore: Save

--- src/components/DrawerComponent/Drawer.ts
+++ src/components/DrawerComponent/Drawer.ts
@@ -1,35 +1,11 @@
-import * as style from "./Drawer.scss";
-
 import { LitElement, customElement } from "lit-element";
 
 import GlobalStyle from "../../GlobalStyle";
-import { Mixin } from "../../../packages/Mixin";
-import { StateMixin } from "../../../packages/StateMixin";
 import Style from "./Style";
-import { connect } from "pwa-helpers/connect-mixin.js";
-import { store } from "../../Store";
 import template from "./DrawerTemplate";
 
-// @customElement("app-drawer")
 @customElement("drawer-component")
 export class Drawer extends LitElement {
-  public firstUpdated() {
-    // Close drawer on link click
-    // const links = this.shadowRoot.querySelectorAll("a");
-    // links.forEach((link: any) =>
-    //   link.addEventListener("click", this._closeDrawer)
-    // );
-  }
-
-  // Handlers
-  public _closeDrawer() {
-    this.dispatchEvent(
-      new CustomEvent("close-drawer", {
-        composed: true
-      })
-    );
-  }
-
   static get styles() {
     return [GlobalStyle, Style];
   }
