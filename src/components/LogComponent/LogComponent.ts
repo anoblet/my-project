52af44def94cbe61adf957a51260a803ecee3d68
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-16 04:07:50 -0500

    Chore: Save

--- src/components/LogComponent/LogComponent.ts
+++ src/components/LogComponent/LogComponent.ts
@@ -1,30 +1,39 @@
-import { LitElement, customElement, html } from "lit-element";
+import { LitElement, css, customElement, html } from "lit-element";
 
 import { getHistory } from "../../Debug";
 
 @customElement("log-component")
 export class Log extends LitElement {
+  public static get styles() {
+    return [css`
+      :host {
+        flex: 1;
+      }
+    `];
+  }
   public render() {
     const history = getHistory();
     return html`
-      <ul>
-        ${history.map(
-          (item: any, index: number) =>
-            html`
-              ${!index
-                ? html`
-                    <li>...${Math.round(item.time)}ms</li>
-                    <li>${item.message}</li>
-                  `
-                : html`
-                    <li>
-                      ${item.message}:
-                      ${Math.round(item.time)}ms (+${Math.round(item.time - history[index - 1].time)}ms)
-                    </li>
-                  `}
-            `
-        )}
-      </ul>
+      <card-component>
+        <ul>
+          ${history.map(
+            (item: any, index: number) =>
+              html`
+                ${!index
+                  ? html`
+                      <li>...${Math.round(item.time)}ms</li>
+                      <li>${item.message}</li>
+                    `
+                  : html`
+                      <li>
+                        ${item.message}: ${Math.round(item.time)}ms
+                        (+${Math.round(item.time - history[index - 1].time)}ms)
+                      </li>
+                    `}
+              `
+          )}
+        </ul>
+      </card-component>
     `;
   }
 }
