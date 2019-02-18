eb759462ec048c87cb17ab2adc22fad31aba2245
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-18 02:31:56 -0500

    Chore: Save

--- src/components/LoadingComponent.ts
+++ src/components/LoadingComponent.ts
@@ -1,19 +1,28 @@
 import { LitElement, css, customElement, html } from "lit-element";
 
 import GlobalStyle from "../GlobalStyle";
-import spinner from "./Loading/Spinner2"
+import spinner from "./Loading/Spinner2";
 
 @customElement("loading-component")
 export class LoadingComponent extends LitElement {
   static get styles() {
     return [
+      css`
+        :host {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+        }
+      `,
       spinner
     ];
   }
 
   public render() {
     return html`
-      <div class="loader"></div>
+      <div class="loader">Test</div>
     `;
   }
 }
