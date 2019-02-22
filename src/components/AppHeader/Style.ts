0677b765a5879d538f3d7c118baceb5190dc0915
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-22 05:49:26 -0500

    Chore: Save

--- src/components/AppHeader/Style.ts
+++ src/components/AppHeader/Style.ts
@@ -3,8 +3,8 @@ import { css } from "lit-element";
 export default css`
   ::slotted(#title) {
     position: absolute;
-    left: 0;
-    right: 0;
+    left: 25%;
+    right: 25%;
     text-align: center;
   }
 `;
