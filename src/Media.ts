4fb4531590c16bf142e9baee82f9fdbf8a80a662
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-23 04:15:47 -0500

    Chore: Save

--- src/Media.ts
+++ src/Media.ts
@@ -1,29 +1,15 @@
 const subscribers: any = [];
-let mediaSize: any;
+
+const mediaChanged = (media: any) => {
+  const mediaSize = media.matches ? "small" : "large";
+  subscribers.map((callback: any) => callback(mediaSize));
+};
 
 const observer = window.matchMedia("(max-width: 500px)");
-export const myListener = (media: any) => {
-  // let mediaSize: string;
-  if (media.matches) {
-    mediaSize = "small";
-  } else {
-    mediaSize = "large";
-  }
-  subscribers.map((callback: any) => callback(mediaSize));
-  return mediaSize;
-};
-observer.addListener(myListener);
-myListener(observer);
 
-export const getMediaSize = () => {
-  myListener(observer);
-};
-
-export const watchMediaSize = (callback: any) =>
-  subscribe(callback);
+observer.addListener(mediaChanged);
+mediaChanged(observer);
 
 export const subscribe = (callback: any) => {
   subscribers.push(callback);
 };
-
-export const watchDevice = () => {};
