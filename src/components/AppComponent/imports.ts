3a3ea3757c614cf42f6e9debe40d5016296c7872
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-24 09:20:05 -0500

    Chore: Save

--- src/components/AppComponent/imports.ts
+++ src/components/AppComponent/imports.ts
@@ -1,12 +1,13 @@
-import(/* webpackChunkName: "MyFlex" */ "../../../packages/my-flex");
-import(/* webpackChunkName: "MyGrid" */ "../../../packages/my-grid");
-import(/* webpackChunkName: "MyLoader" */ "../../../packages/my-loader");
-import(/* webpackChunkName: "MyCard" */ "../../../packages/my-card");
+import(/* webpackChunkName: "MyFlex" */ "../../../packages/my-flex/MyFlex");
+import(/* webpackChunkName: "MyGrid" */ "../../../packages/my-grid/MyGrid");
+import(/* webpackChunkName: "MyLoader" */ "../../../packages/my-loader/MyLoader");
+import(/* webpackChunkName: "MyCard" */ "../../../packages/my-card/MyCard");
 import(/* webpackChunkName: "MWCButton" */ "@material/mwc-button");
 import(/* webpackChunkName: "MWCIcon" */ "@material/mwc-icon");
 import(/* webpackChunkName: "MWCFab" */ "@material/mwc-fab");
 import(/* webpackChunkName: "AppHeader" */ "../AppHeader/AppHeader");
-import(/* webpackChunkName: "AppFooter" */ "../AppFooter/AppFooter"); // @deprecated import(/* webpackChunkName: "AppTheme" */ "../AppTheme/AppTheme"),
+import(/* webpackChunkName: "AppFooter" */ "../AppFooter/AppFooter");
+// @deprecated import(/* webpackChunkName: "AppTheme" */ "../AppTheme/AppTheme"),
 import(/* webpackChunkName: "Drawer" */ "../DrawerComponent/Drawer");
 import(/* webpackChunkName: "ProfileMenu" */ "../ProfileMenu/ProfileMenu"); // @deprecated
 import(/* webpackChunkName: "AdminComponent" */ "../AdminComponent/AdminComponent");
