6d0620c77f3e552a3d6a5da1443349ba44c6304b
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-24 14:44:43 -0500

    Chore: Save

--- src/components/AppComponent/Routes.ts
+++ src/components/AppComponent/Routes.ts
@@ -12,43 +12,43 @@ export const routes = [
     path: "/admin",
     component: "admin-component",
     src: () =>
-      import(/* webpackChunkName: "AdminComponent" */ /* webpackPrefetch: true */ "../AdminComponent/AdminComponent")
+      import(/* webpackChunkName: "AdminComponent" */ "../AdminComponent/AdminComponent")
   },
   {
     path: "/contact",
     component: "contact-component",
     src: () =>
-      import(/* webpackChunkName: "PageContact" */ /* webpackPrefetch: true */ "../Contact/Contact")
+      import(/* webpackChunkName: "PageContact" */ "../Contact/Contact")
   },
   {
     path: "/components/:component?",
     component: "page-components",
     src: () =>
-      import(/* webpackChunkName: "PageComponents" */ /* webpackPrefetch: true */ "../PageComponents/PageComponents")
+      import(/* webpackChunkName: "PageComponents" */ "../PageComponents/PageComponents")
   },
   {
     path: "/post",
     component: "post-controller",
     src: () =>
-      import(/* webpackChunkName: "PostController" */ /* webpackPrefetch: true */ "../Post/PostController")
+      import(/* webpackChunkName: "PostController" */ "../Post/PostController")
   },
   {
     path: "/post/:action?",
     component: "post-controller",
     src: () =>
-      import(/* webpackChunkName: "PostController" */ /* webpackPrefetch: true */ "../Post/PostController")
+      import(/* webpackChunkName: "PostController" */ "../Post/PostController")
   },
   {
     path: "/post/read/:id?",
     component: "post-component",
     src: () =>
-      import(/* webpackChunkName: "PostComponent" */ /* webpackPrefetch: true */ "../PostComponent/PostComponent")
+      import(/* webpackChunkName: "PostComponent" */ "../PostComponent/PostComponent")
   },
   {
     path: "/post/list",
     component: "post-grid-component",
     src: () =>
-      import(/* webpackChunkName: "PostGridComponent" */ /* webpackPrefetch: true */ "../Post/PostGridComponent")
+      import(/* webpackChunkName: "PostGridComponent" */ "../Post/PostGridComponent")
   },
   // {
   //   path: "/post/:action/:id?",
@@ -60,48 +60,48 @@ export const routes = [
     path: "/user/:action?",
     component: "user-controller",
     src: () =>
-      import(/* webpackChunkName: "UserController" */ /* webpackPrefetch: true */ "../User/UserController")
+      import(/* webpackChunkName: "UserController" */ "../User/UserController")
   },
   {
     path: "/user-settings",
     component: "settings-component",
     src: () =>
-      import(/* webpackChunkName: "SettingsComponent" */ /* webpackPrefetch: true */ "../User/SettingsComponent")
+      import(/* webpackChunkName: "SettingsComponent" */ "../User/SettingsComponent")
   },
   {
     path: "/user-theme",
     component: "theme-component",
     src: () =>
-      import(/* webpackChunkName: "ThemeComponent" */ /* webpackPrefetch: true */ "../ThemeComponent/ThemeComponent")
+      import(/* webpackChunkName: "ThemeComponent" */ "../ThemeComponent/ThemeComponent")
   },
   {
     path: "/issue",
     component: "issue-component",
     src: () =>
-      import(/* webpackChunkName: "IssueComponent" */ /* webpackPrefetch: true */ "../IssueComponent/IssueComponent")
+      import(/* webpackChunkName: "IssueComponent" */ "../IssueComponent/IssueComponent")
   },
   {
     path: "/readme",
     component: "readme-component",
     src: () =>
-      import(/* webpackChunkName: "ReadmeComponent" */ /* webpackPrefetch: true */ "../Readme/ReadmeComponent")
+      import(/* webpackChunkName: "ReadmeComponent" */ "../Readme/ReadmeComponent")
   },
   {
     path: "/sitemap",
     component: "sitemap-component",
     src: () =>
-      import(/* webpackChunkName: "SitemapComponent" */ /* webpackPrefetch: true */ "../Sitemap/Component")
+      import(/* webpackChunkName: "SitemapComponent" */ "../Sitemap/Component")
   },
   {
     path: "/blog",
     component: "blog-component",
     src: () =>
-      import(/* webpackChunkName: "BlogComponent" */ /* webpackPrefetch: true */ "../BlogComponent/Blog")
+      import(/* webpackChunkName: "BlogComponent" */ "../BlogComponent/Blog")
   },
   {
     path: "/timings",
     component: "log-component",
     src: () =>
-      import(/* webpackChunkName: "LogComponent" */ /* webpackPrefetch: true */ "../LogComponent/LogComponent")
+      import(/* webpackChunkName: "LogComponent" */ "../LogComponent/LogComponent")
   }
 ];
