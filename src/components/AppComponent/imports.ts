b6c550d0ca53e09d4312806d40a4a0a7af98867c
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-25 13:02:43 -0500

    Chore: Save

--- src/components/AppComponent/imports.ts
+++ src/components/AppComponent/imports.ts
@@ -1,17 +1,13 @@
-// import(/* webpackChunkName: "MyGrid" */ "../../../packages/my-grid/MyGrid");
-// import(/* webpackChunkName: "MyLoader" */ "../../../packages/my-loader/MyLoader");
-// import(/* webpackChunkName: "MyCard" */ "../../../packages/my-card/MyCard");
-import(/* webpackChunkName: "MWCButton" */ "@material/mwc-button");
-import(/* webpackChunkName: "MWCIcon" */ "@material/mwc-icon");
-import(/* webpackChunkName: "MWCFab" */ "@material/mwc-fab");
 import(/* webpackChunkName: "AppHeader" */ "../AppHeader/AppHeader");
 import(/* webpackChunkName: "AppFooter" */ "../AppFooter/AppFooter");
+import(/* webpackChunkName: "MWCButton" */ "@material/mwc-button");
+import(/* webpackChunkName: "MWCFab" */ "@material/mwc-fab");
 import(/* webpackChunkName: "Drawer" */ "../DrawerComponent/Drawer");
 import(/* webpackChunkName: "ProfileMenu" */ "../ProfileMenu/ProfileMenu"); // @deprecated
 import(/* webpackChunkName: "Breadcrumb" */ "../BreadcrumbComponent/Breadcrumb");
 import(/* webpackChunkName: "MediaQuery" */ "../../../packages/MediaQuery");
 import(/* webpackChunkName: "MenuComponent" */ "../MenuComponent/MenuComponent");
-import(/* webpackChunkName: "LoadingComponent" */ "../LoadingComponent");
+// import(/* webpackChunkName: "LoadingComponent" */ "../LoadingComponent");
 import(/* webpackChunkName: "CardComponent" */ "../CardComponent/CardComponent");
 import(/* webpackChunkName: "GridComponent" */ "../GridComponent/GridComponent");
 
