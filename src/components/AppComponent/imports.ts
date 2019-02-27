8c54b751fe55f4f0f011b828dbf50e74be799c97
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-27 15:42:16 -0500

    Chore: Save

--- src/components/AppComponent/imports.ts
+++ src/components/AppComponent/imports.ts
@@ -9,6 +9,5 @@ import "../BreadcrumbComponent/Breadcrumb";
 import "../MenuComponent/MenuComponent";
 import "../CardComponent/CardComponent";
 import "../GridComponent/GridComponent";
-import(/* webpackChunkName: "Firebase" */ "firebase/app");
-import(/* webpackChunkName: "FirebaseAuth" */ "firebase/auth");
+
 export default true;
