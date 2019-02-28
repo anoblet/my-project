cf332f65184fbf5bac0a53401adf2afb3756eaec
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-28 09:12:03 -0500

    Chore: Save

--- src/components/AppComponent/Imports.ts
+++ src/components/AppComponent/Imports.ts
@@ -1,11 +1,8 @@
 import "../AppHeader/AppHeader";
 import "../AppFooter/AppFooter";
 import "../PageStatic";
-// import "@material/mwc-button";
-// import "@material/mwc-fab";
 import "../DrawerComponent/Drawer";
-import "../ProfileMenu/ProfileMenu"; // @deprecated
-import "../BreadcrumbComponent/Breadcrumb";
+import "../ProfileMenu/ProfileMenu";
 import "../MenuComponent/MenuComponent";
 import "../CardComponent/CardComponent";
 import "../GridComponent/GridComponent";
