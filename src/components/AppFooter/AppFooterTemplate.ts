1a329f0c72713898d56763af78deb3e1771b408b
Andrew Noblet <andrewbnoblet@gmail.com>
2019-02-28 07:10:36 -0500

    Chore: Save

--- src/components/AppFooter/AppFooterTemplate.ts
+++ src/components/AppFooter/AppFooterTemplate.ts
@@ -2,18 +2,26 @@ import { html } from "lit-element";
 
 export default function() {
   return html`
-    <a
-      href="https://github.com/anoblet/my-project"
-      rel="noreferrer"
-      target="_blank"
-    >
-      <i class="material-icons" @click="${this._toggleDrawer}">code</i>
-    </a>
-    <a href="/">
-      <i class="material-icons" @click="${this._toggleDrawer}">home</i>
-    </a>
-    <a href="mailto: andrewbnoblet@gmail.com">
-      <i class="material-icons" @click="${this._toggleDrawer}">mail_outline</i>
-    </a>
+    <span class="item">
+      <a
+        href="https://github.com/anoblet/my-project"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <i class="material-icons" @click="${this._toggleDrawer}">code</i>
+      </a>
+    </span>
+    <span id="home" class="item">
+      <a href="/">
+        <i class="material-icons" @click="${this._toggleDrawer}">home</i>
+      </a>
+    </span>
+    <span class="item">
+      <a href="mailto: andrewbnoblet@gmail.com">
+        <i class="material-icons" @click="${this._toggleDrawer}"
+          >mail_outline</i
+        >
+      </a>
+    </span>
   `;
 }
--- src/components/AppFooter/Style.ts
+++ src/components/AppFooter/Style.ts
@@ -8,8 +8,26 @@ export default css`
     align-items: center;
   }
 
-  :host > * {
+  .material-icons {
+    display: flex;
+    color: var(--background-color);
+  }
+
+  .item {
     padding: 1em;
+    background: var(--secondary-color);
+    color: var(--background-color);
+    border-radius: 50%;
+    margin: 1em;
+  }
+
+  .item:not(#home) {
+    margin: 0.5em;
+    opacity: 0.33;
+  }
+
+  .item:not(#home):hover {
+    opacity: 1;
   }
 
   mwc-fab {
