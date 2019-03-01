553cffb8faaa3e44b05801d9343fac37fad98341
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-01 06:28:16 -0500

    Chore: Save

--- src/components/Readme/ReadmeComponent.ts
+++ src/components/Readme/ReadmeComponent.ts
@@ -1,11 +1,22 @@
-import { LitElement, customElement, property } from "lit-element";
-import { getReadme } from "./Readme";
+import { LitElement, customElement, html, property } from "lit-element";
+import { unsafeHTML } from "lit-html/directives/unsafe-html";
+import { mdToHtml } from "../../../packages/Showdown/Showdown";
 
 @customElement("readme-component")
 class Readme extends LitElement {
-  @property() public path: string = "https://raw.githubusercontent.com/anoblet/my-project/master/README.md";
+  @property() public path: string =
+    "https://raw.githubusercontent.com/anoblet/my-project/master/README.md";
+  @property() public html: string;
+
+  public async beforeRender() {
+    const data = await fetch(this.path).then((response: any) => response.text());
+    this.html = mdToHtml(data)
+  }
 
   public render() {
-    return getReadme(this.path);
+    return html`
+      ${unsafeHTML(this.html)}
+    `;
+    // return getReadme(this.path);
   }
 }
