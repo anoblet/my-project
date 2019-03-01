8bcfc31184902b585ac944081471f3a259425494
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-01 06:35:37 -0500

    Chore: Save

--- src/components/AdminComponent/AdminComponent.ts
+++ src/components/AdminComponent/AdminComponent.ts
@@ -3,9 +3,6 @@ import { LitElement, customElement, css } from "lit-element";
 import GlobalStyle from "../../GlobalStyle";
 import template from "./AdminTemplate";
 
-// import(/* webpackChunkName: "CardComponent" */ "../CardComponent/CardComponent");
-// import(/* webpackChunkName: "GridComponent" */ "../GridComponent/GridComponent");
-
 @customElement("admin-component")
 export class AdminComponent extends LitElement {
   static get styles() {
