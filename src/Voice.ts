ba234f8a0a6602e051d098ecd7692e109e52ebac
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-02 16:08:55 -0500

    Chore: Save

--- src/Voice.ts
+++ src/Voice.ts
@@ -2,28 +2,11 @@ import { navigate } from "./Router";
 import { routes } from "./components/AppComponent/Routes";
 
 const annyang = require("annyang");
-annyang.debug();
 
 const commands: any = {};
 routes.map((route: any) => {
   if (route.voice) commands[route.voice] = () => navigate(route.path);
 });
-
-// Let's define a command.
-// const commands: any = {
-//   home: () => {
-//     navigate("/");
-//   },
-//   "eye exam": () => {
-//     navigate("/components/eye-exam");
-//   },
-//   settings: () => {
-//     navigate("/user-settings");
-//   },
-//   theme: () => {
-//     navigate("/user-theme");
-//   }
-// };
 
 annyang.addCommands(commands);
 
