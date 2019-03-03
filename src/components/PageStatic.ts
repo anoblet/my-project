a5cc6d50755284d4b52484730b571abce2e8b6cb
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-02 19:55:17 -0500

    Chore: Save

--- src/components/PageStatic.ts
+++ src/components/PageStatic.ts
@@ -2,19 +2,19 @@ import { LitElement, css, customElement, html, property } from "lit-element";
 import GlobalStyle from "../GlobalStyle";
 import "./PatreonComponent";
 import "./Chart/ChartComponent";
+
+const Style = css`
+  :host {
+    flex: 1;
+  }
+`;
+
 @customElement("page-static")
 class PageStatic extends LitElement {
   @property() public template: any;
 
   static get styles() {
-    return [
-      GlobalStyle,
-      css`
-        :host {
-          flex: 1;
-        }
-      `
-    ];
+    return [GlobalStyle, Style];
   }
 
   public render() {
@@ -22,8 +22,8 @@ class PageStatic extends LitElement {
       <grid-component>
         <card-component title="Welcome">
           If you're looking for content go to
-          <a href="/blog">our blog</a>. To view the timings of the app, visit
-          <a href="/timings">timings</a>.
+          <a href="/blog">our blog</a>. To view the performance of the app,
+          visit <a href="/timings">timings</a>.
         </card-component>
         <card-component title="Patreon">
           <patreon-component></patreon-component>
