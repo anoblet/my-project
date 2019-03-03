126ef2894b62a378fcabff8ee33a666ff19f102d
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-03 11:33:49 -0500

    Chore: Save

--- src/BaseElement.ts
+++ src/BaseElement.ts
@@ -1,31 +1,4 @@
 import { LitElement, property } from "lit-element";
 
 export class BaseElement extends LitElement {
-  @property() private loaded = false;
-  public connectedCallback() {
-    super.connectedCallback();
-    this.beforeLoaded().then(() => {
-      this.loaded = true;
-    });
-  }
-
-  private async beforeLoaded() {
-    return;
-  }
-
-  protected shouldUpdate(changedProperties: any) {
-    if (this.loaded) return super.shouldUpdate(changedProperties);
-    else {
-      return false;
-    }
-  }
-
-  private _setDefaultValue() {
-    // @ts-ignore
-    const properties = this.constructor.properties;
-    Object.keys(properties).map((key: string) => {
-      // @ts-ignore
-      if (properties[key].value) this[key] = properties[key].value;
-    });
-  }
 }
