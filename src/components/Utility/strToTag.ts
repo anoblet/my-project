27bf5105d06f5df00e58a5cc4451132a1944d0c6
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-04 09:08:08 -0500

    Chore: Save

--- src/components/Utility/strToTag.ts
+++
@@ -1,4 +0,0 @@
-// Using a regular function instead of an arrow function allow us to bind (this) and retain context
-export const strToTag = function(str: string, tag: any) {
-  const result = new Function("str", "tag", "return " + tag + "`" + str + "`")();
-};
