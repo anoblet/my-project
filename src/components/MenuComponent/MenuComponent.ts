dcb78dd03d35b9fc011169ef2a7f5b88905db7f3
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-04 14:04:39 -0500

    Chore: Save

--- src/components/MenuComponent/MenuComponent.ts
+++ src/components/MenuComponent/MenuComponent.ts
@@ -2,28 +2,31 @@ import * as style from "./MenuComponent.scss";
 
 import { LitElement, html, property } from "lit-element";
 
-import { Mixin } from "../../../packages/Mixin";
-import { FirebaseMixin } from "../../../packages/FirebaseMixin";
-import { StateMixin } from "../../../packages/StateMixin";
-import template from "./MenuTemplate";
-import { connect } from "pwa-helpers/connect-mixin.js";
-import { store } from "../../Store";
 import GlobalStyle from "../../GlobalStyle";
+import { store } from "../../Store";
+import template from "./MenuTemplate";
 
-export class MenuComponent extends Mixin(connect(store)(LitElement), [
-  FirebaseMixin,
-  StateMixin
-]) {
+export class MenuComponent extends LitElement {
   @property({ type: Boolean, reflect: true }) public hidden = true;
+  public boundListener: any;
 
   constructor() {
     super();
-    this.boundListener = this._onContextMenu.bind(this);
+    store.subscribe(() => {
+      const state = store.getState();
+      if (state.settings) {
+        if (state.settings.rightClick) {
+          document.addEventListener("contextmenu", this.boundListener);
+        } else {
+          document.removeEventListener("contextmenu", this.boundListener);
+        }
+      }
+    });
     document.addEventListener("keyup", this._onKeyUp.bind(this));
   }
 
-  public firstUpdated() {
-    super.firstUpdated();
+  public firstUpdated(changedProperties: any) {
+    super.firstUpdated(changedProperties);
 
     const links = this.shadowRoot.querySelectorAll("a");
     links.forEach((link: any) => {
@@ -39,18 +42,8 @@ export class MenuComponent extends Mixin(connect(store)(LitElement), [
   }
 
   public _onKeyUp(e: any) {
-    if (e.shiftKey && e.keyCode == 32) {
+    if (e.shiftKey && e.keyCode === 32) {
       this.hidden = !this.hidden;
-    }
-  }
-
-  public stateChanged(state: any) {
-    if (state.settings) {
-      if (state.settings.rightClick) {
-        document.addEventListener("contextmenu", this.boundListener);
-      } else {
-        document.removeEventListener("contextmenu", this.boundListener);
-      }
     }
   }
 
