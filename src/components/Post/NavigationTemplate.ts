89af70cacb6f189bdd0281f4e9b3b882c11a3690
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-04 14:33:38 -0500

    Chore: Save

--- src/components/Post/NavigationTemplate.ts
+++
@@ -1,8 +0,0 @@
-import { html } from "lit-element";
-
-export default function() {
-  return html`
-    <a href="/post/create"><mwc-button outlined>New</mwc-button></a>
-    <a href="/post/read"><mwc-button outlined>List</mwc-button></a>
-  `;
-}
