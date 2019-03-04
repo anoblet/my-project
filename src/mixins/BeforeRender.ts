f07b05df64ecc6daf9a565c561e64d956ed39432
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-04 12:24:50 -0500

    Chore: Save

--- src/mixins/BeforeRender.ts
+++ src/mixins/BeforeRender.ts
@@ -4,11 +4,17 @@ type Constructor<T = {}> = new (...args: any[]) => T;
 
 export function BeforeRender<B extends Constructor<HTMLElement>>(baseClass: B) {
   class Mixin extends baseClass {
-    @property({ type: String, reflect: true, attribute: "media-size" })
-    public mediaSize: string;
+    @property({ type: Boolean })
+    public beforeRenderComplete: boolean;
 
     constructor(...args: any[]) {
       super(...args);
+      this.beforeRenderComplete = false;
+      this.beforeRender().then(() => (this.beforeRenderComplete = true));
+    }
+
+    public async beforeRender() {
+      return;
     }
   }
 
