1cda3be0645bdfe3e9a4e8b42633a4f37a911fcd
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-06 20:20:09 -0500

    Chore: Save

--- src/components/Dashboard/DashboardComponent.ts
+++ src/components/Dashboard/DashboardComponent.ts
@@ -1,8 +1,27 @@
-import { LitElement, customElement, html } from "lit-element";
+import { LitElement, customElement, html, property } from "lit-element";
+// import "../GridComponent/GridComponent";
+
+export interface DashboardComponent {
+  src: string;
+}
+
+/**
+ * Chromecast idle screen replacement
+ *
+ * @param  "dasboard-component" [description]
+ * @return                      [description]
+ */
 
 @customElement("dasboard-component")
 export class Dashboard extends LitElement {
+  @property() public itemArray: any;
   public render() {
-    return html``;
+    return html`
+      <grid-component
+        >${this.itemArray.map((item: any) => {
+          return;
+        })}</grid-component
+      >
+    `;
   }
 }
