54258b42b4de2a0fae14df3e1a5a7eb8c3a41217
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-08 01:44:48 -0500

    Chore: Save

--- src/components/Drawer/test/drawer.test.js
+++ src/components/Drawer/test/drawer.test.js
@@ -3,22 +3,8 @@ import { html, fixture, expect } from "@open-wc/testing";
 import "../DrawerComponent";
 
 describe("<drawer-component>", () => {
-  it("media size is determined, what to do next?", async () => {
+  it("drawer shows up", async () => {
     const el = await fixture("<drawer-component></drawer-component>");
-    expect(!!el.mediaSize).to.equal(true);
-  });
-
-  // it("drawer is closed on mobile screens", async () => {
-  //   const el = await fixture(
-  //     "<drawer-component media-size='mobile'></drawer-component>"
-  //   );
-  //   expect(el.hidden).to.equal(true);
-  // });
-
-  it("drawer is displayed by default on desktop screens", async () => {
-    const el = await fixture(
-      "<drawer-component media-size='desktop'></drawer-component>"
-    );
-    expect(el.hidden).to.equal(false);
+    expect(!!el).to.equal(true);
   });
 });
