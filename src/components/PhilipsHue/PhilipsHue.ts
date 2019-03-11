7d64de6a3fdf2f9d2519efa06b94ed930d1ab80f
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-11 05:17:13 -0400

    Chore: Save

--- src/components/PhilipsHue/PhilipsHue.ts
+++ src/components/PhilipsHue/PhilipsHue.ts
@@ -1,20 +1,20 @@
 export default async () => {
-  const ip = await fetch("https://discovery.meethue.com/")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-      return myJson[0]["internalipaddress"];
-    });
-  await fetch(`http://${ip}/api/newdeveloper`, {
-    method: "GET",
-    mode: "no-cors"
-  })
-    .then(function(response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function(myJson) {
-      return myJson;
-    });
+  // const ip = await fetch("https://discovery.meethue.com/")
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(myJson) {
+  //     return myJson[0]["internalipaddress"];
+  //   });
+  // await fetch(`http://${ip}/api/newdeveloper`, {
+  //   method: "GET",
+  //   mode: "no-cors"
+  // })
+  //   .then(function(response) {
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .then(function(myJson) {
+  //     return myJson;
+  //   });
 };
