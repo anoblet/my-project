4fbf3486a276f0bf122dbf42b7b0bac073e91926
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-11 05:45:01 -0400

    Chore: Save

--- src/components/PhilipsHue/PhilipsHue.ts
+++ src/components/PhilipsHue/PhilipsHue.ts
@@ -1,20 +1,28 @@
 export default async () => {
-  // const ip = await fetch("https://discovery.meethue.com/")
-  //   .then(function(response) {
-  //     return response.json();
-  //   })
-  //   .then(function(myJson) {
-  //     return myJson[0]["internalipaddress"];
-  //   });
-  // await fetch(`http://${ip}/api/newdeveloper`, {
-  //   method: "GET",
-  //   mode: "no-cors"
-  // })
-  //   .then(function(response) {
-  //     console.log(response);
-  //     return response.json();
-  //   })
-  //   .then(function(myJson) {
-  //     return myJson;
-  //   });
+  const ip = await findBridge();
+  const _auth = await auth(ip);
+};
+
+const findBridge = async () => {
+  return await fetch("https://discovery.meethue.com/")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      return myJson[0]["internalipaddress"];
+    });
+};
+
+const auth = async (ip: string) => {
+  await fetch(`http://${ip}/api/newdeveloper`, {
+    method: "GET",
+    mode: "no-cors"
+  })
+    .then(function(response) {
+      // const result = response ? response.json() : false;
+      // return result;
+    })
+    .then(function(myJson) {
+      return myJson;
+    });
 };
