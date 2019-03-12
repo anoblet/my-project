0cf5aea40021c9222f5727646c707268660fc760
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-12 09:38:35 -0400

    Chore: Save

--- src/components/Dashboard/Template.ts
+++ src/components/Dashboard/Template.ts
@@ -5,52 +5,92 @@ export default function() {
     <div class="grid">
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/195/400/any?1" />
+          <img
+            src="https://placeimg.com/195/400/any?1"
+            width="195"
+            height="400"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/400/195/any?2" />
+          <img
+            src="https://placeimg.com/400/195/any?2"
+            width="400"
+            height="195"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/195/400/any?3" />
+          <img
+            src="https://placeimg.com/195/400/any?3"
+            width="195"
+            height="400"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/400/195/any?4" />
+          <img
+            src="https://placeimg.com/400/195/any?4"
+            width="400"
+            height="195"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/195/400/any?5" />
+          <img
+            src="https://placeimg.com/195/400/any?5"
+            width="195"
+            height="400"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/400/195/any?6" />
+          <img
+            src="https://placeimg.com/400/195/any?6"
+            width="400"
+            height="195"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/195/400/any?7" />
+          <img
+            src="https://placeimg.com/195/400/any?7"
+            width="195"
+            height="400"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/400/195/any?8" />
+          <img
+            src="https://placeimg.com/400/195/any?8"
+            width="400"
+            height="195"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/195/400/any?9" />
+          <img
+            src="https://placeimg.com/195/400/any?9"
+            width="195"
+            height="400"
+          />
         </div>
       </div>
       <div class="item">
         <div class="item-content">
-          <img src="https://placeimg.com/400/195/any?10" />
+          <img
+            src="https://placeimg.com/400/195/any?10"
+            width="400"
+            height="195"
+          />
         </div>
       </div>
     </div>
