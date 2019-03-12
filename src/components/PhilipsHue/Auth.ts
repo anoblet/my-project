d75d80107ede9195831b31407875058a79d9db2d
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-11 22:40:43 -0400

    Chore: Save

--- src/components/PhilipsHue/Auth.ts
+++ src/components/PhilipsHue/Auth.ts
@@ -1,10 +1,10 @@
 import { LitElement, html, property } from "lit-element";
 import { BeforeRender } from "../../mixins/BeforeRender";
-import { auth } from "./PhilipsHue";
 
 export class Auth extends BeforeRender(LitElement) {
   @property() id: any;
   @property() ip: any;
+  @property() state: any; // 0 = No interraction, 1 = User pressed connect, and needs to press physical button, 2 = User pressed physical button and we need username
 
   async beforeRender() {
     return;
