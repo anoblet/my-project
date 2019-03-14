635212f2097d82afdf5140195667f3bb662bef37
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-14 14:20:00 -0400

    Chore: Save

---
+++ src/components/Contacts/Component.ts
@@ -0,0 +1,15 @@
+import { LitElement, customElement } from "lit-element";
+
+import GlobalStyle from "../../GlobalStyle";
+import Style from "./Style";
+import Template from "./Template";
+
+@customElement("contact-component")
+export class Contact extends LitElement {
+  static get styles() {
+    return [GlobalStyle, Style];
+  }
+  public render() {
+    return Template.bind(this)();
+  }
+}
