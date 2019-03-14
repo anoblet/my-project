a4476aa678606801de56c9a7ad1f2b435af8d97b
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-14 19:23:58 -0400

    Chore: Save

--- src/components/Contacts/Template.ts
+++ src/components/Contacts/Template.ts
@@ -4,16 +4,24 @@ import("../Button/ButtonComponent");
 export default function() {
   return html`
     <card-component title="Contacts">
-      <grid-component id="buttons">
-        <button-component label="In" @click=${this.in}></button-component>
-        <button-component label="Out" @click=${this.out}></button-component>
-      </grid-component>
       <grid-component>
-        ${this.data.log.map(
-          entry => html`
-            <div>${entry.type} ${new Date(entry.time).toLocaleString()}</div>
-          `
-        )}
+        <card-component>
+          <grid-component id="buttons">
+            <button-component label="In" @click=${this.in}></button-component>
+            <button-component label="Out" @click=${this.out}></button-component>
+          </grid-component>
+        </card-component>
+        <card-component>
+          <grid-component id="log">
+            ${this.data.log.map(
+              entry => html`
+                <div>
+                  ${entry.type} ${new Date(entry.time).toLocaleString()}
+                </div>
+              `
+            )}
+          </grid-component>
+        </card-component>
       </grid-component>
     </card-component>
   `;
