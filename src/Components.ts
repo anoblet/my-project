5364eb5d8929cea4838598b3db9837dffdebf8a2
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-20 10:38:09 -0400

    Chore: Save

--- src/Components.ts
+++ src/Components.ts
@@ -2,49 +2,49 @@ export const components = [
   {
     name: "Voice",
     src: () =>
-      import(/* webpackChunkName: Voice*/ "./components/VoiceComponent"),
+      import(/* webpackChunkName: "Voice" */ "./components/VoiceComponent"),
     tag: "voice-component"
   },
   {
     name: "Collection List",
     src: () =>
-      import(/* webpackChunkName: ComponentList*/ "./components/Collection/CollectionList"),
+      import(/* webpackChunkName: "ComponentList" */ "./components/Collection/CollectionList"),
     tag: "collection-list"
   },
   {
     name: "Philips Hue",
     src: () =>
-      import(/* webpackChunkName: PhilipsHue */ "./components/PhilipsHue/LightsComponent"),
+      import(/* webpackChunkName: "PhilipsHue" */ "./components/PhilipsHue/LightsComponent"),
     tag: "lights-component"
   },
   {
     name: "Philips Hue Demo",
-    src: () => import(/* webpackChunkName: PhilipsHueDemo */ "./components/PhilipsHue/Demo"),
+    src: () => import(/* webpackChunkName: "PhilipsHueDemo" */ "./components/PhilipsHue/Demo"),
     tag: "philips-hue-demo"
   },
   {
     name: "Eye Exam",
-    src: () => import(/* webpackChunkName: EyeExam */ "./components/EyeExam/EyeExamComponent"),
+    src: () => import(/* webpackChunkName: "EyeExam" */ "./components/EyeExam/EyeExamComponent"),
     tag: "eye-exam"
   },
   {
     name: "Color Picker",
-    src: () => import(/* webpackChunkName: ColorPicker */ "./components/ColorPicker"),
+    src: () => import(/* webpackChunkName: "ColorPicker" */ "./components/ColorPicker"),
     tag: "color-picker"
   },
   {
     name: "Dashboard",
-    src: () => import(/* webpackChunkName: Dashboard */ "./components/Dashboard/Component"),
+    src: () => import(/* webpackChunkName: "Dashboard" */ "./components/Dashboard/Component"),
     tag: "dashboard-component"
   },
   {
     name: "Contacts",
-    src: () => import(/* webpackChunkName: Contacts */ "./components/Contacts/Component"),
+    src: () => import(/* webpackChunkName: "Contacts" */ "./components/Contacts/Component"),
     tag: "contacts-component"
   },
   {
     name: "Muuri",
-    src: () => import(/* webpackChunkName: Muuri */ "./components/muuri/component"),
+    src: () => import(/* webpackChunkName: "Muuri" */ "./components/muuri/component"),
     tag: "muuri-component"
   }
 ];
