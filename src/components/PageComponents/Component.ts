3ab7dc0223e29568e10ccc075dcb0c5bbea3bdcc
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-22 20:11:16 -0400

    Chore: Save

--- src/components/PageComponents/Component.ts
+++ src/components/PageComponents/Component.ts
@@ -6,17 +6,6 @@ import style from "./PageComponentsStyle";
 import template from "./PageComponentsTemplate";
 import { unsafeHTML } from "lit-html/directives/unsafe-html";
 
-import(/* webpackChunkName: "EyeExam" */ "../EyeExam/EyeExamComponent");
-import(/* webpackChunkName: "WebSpeech" */ "../WebSpeech/WebSpeechComponent");
-import(/* webpackChunkName: "PropertyEditor" */ "../PropertyEditor/PropertyEditor");
-import("../Collection/CollectionList");
-import("../ColorPicker");
-import("../Dashboard/Component");
-import("../PhilipsHue/LightsComponent");
-import("../PhilipsHue/Demo");
-import("../Contacts/Component");
-import("../muuri/component");
-
 export class PageComponents extends LitElement {
   @property() public component: string;
   @property() public components: unknown;
