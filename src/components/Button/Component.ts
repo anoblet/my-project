08938cac83b2c7f5dfdc5b7ac58281a58c636d5b
Andrew Noblet <andrewbnoblet@gmail.com>
2019-03-26 22:32:27 -0400

    Chore: Save

--- src/components/Button/Component.ts
+++ src/components/Button/Component.ts
@@ -5,14 +5,10 @@ import Properties from "./Properties";
 
 @customElement("button-component")
 export class ButtonComponent extends LitElement {
-  @property() public theme: string = "native";
+  @property({ type: String }) theme = "native";
+
   static properties = Properties;
   static styles = Style;
-
-  public constructor() {
-    super();
-    this.theme = "native";
-  }
 
   public render() {
     return Template.bind(this)();
