f9fc969d8d380902d604a87d0b0cf44dd35332fb
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-04 09:34:06 -0400

    Chore: Save

--- src/components/Card/Template.ts
+++ src/components/Card/Template.ts
@@ -2,22 +2,22 @@ import { html } from "lit-element";
 
 export default function() {
   return html`
-    ${this.title
-      ? html`
-          <div id="title">
+    <div id="title">
+      ${this.title
+        ? html`
             <h3>${this.title}</h3>
-            ${this.collapsible
-              ? html`
-                  <i class="material-icons"
-                    >${this.collapsed ? "expand_more" : "expand_less"}</i
-                  >
-                `
-              : ""}
-          </div>
-        `
-      : html`
-          <slot name="title"></slot>
-        `}
+          `
+        : html`
+            <slot name="title"></slot>
+          `}
+      ${this.collapsible
+        ? html`
+            <i class="material-icons"
+              >${this.collapsed ? "expand_less" : "expand_more"}</i
+            >
+          `
+        : ""}
+    </div>
     <div id="content"><slot name="content"> </slot><slot> </slot></div>
     <slot name="actions"></slot>
   `;
