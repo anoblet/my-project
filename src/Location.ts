89f610a10378f593af38aad20dc0fcb86f572c10
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-05 15:24:32 -0400

    Chore: Save

--- src/Location.ts
+++ src/Location.ts
@@ -1,10 +1,10 @@
-const geoSuccess = () => {
-  return;
-};
-const geoError = () => {
-  return;
-};
-
 export const getLocation = () => {
-  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  return new Promise((resolve: any) => {
+    navigator.geolocation.getCurrentPosition((position: any) => {
+      resolve({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    });
+  });
 };
--- src/components/Weather/Component.ts
+++ src/components/Weather/Component.ts
@@ -16,7 +16,49 @@ export class Component extends LitElement {
     return Template.bind(this)();
   }
 
-  public getLocation() {
-    return getLocation();
+  public async getLocation() {
+    const location: any = await getLocation();
+    console.log(location);
+    const result = await fetch(
+      `https://api.weather.gov/points/${location.latitude},${
+        location.longitude
+      }`
+    ).then(function(response) {
+      return response.json();
+    });
+    const forecast = await fetch(result.properties.forecast)
+      .then((response: any) => {
+        return response.json();
+      })
+      .then((json: any) => {
+        return json.properties.periods[0].temperature;
+      });
+    console.log(forecast);
+  }
+
+  public async getTemperature() {
+    const period = await this.getPeriod();
+    return period.temperature;
+  }
+
+  public async getPeriod() {
+    const location: any = await getLocation();
+    return await fetch(
+      `https://api.weather.gov/points/${location.latitude},${
+        location.longitude
+      }`
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(async (result: any) => {
+        return fetch(result.properties.forecast)
+          .then((response: any) => {
+            return response.json();
+          })
+          .then((json: any) => {
+            return json.properties.periods[0];
+          });
+      });
   }
 }
