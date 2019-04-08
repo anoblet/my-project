63bc62e5321e9adad5ec81ed04d16220f90e903b
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-08 16:51:48 -0400

    Chore: Save

--- src/components/Playground/Component.ts
+++ src/components/Playground/Component.ts
@@ -1,6 +1,5 @@
 import { LitElement, customElement, property } from "lit-element";
 
-import GlobalStyle from "../../GlobalStyle";
 import Style from "./Style";
 import Template from "./Template";
 
@@ -8,7 +7,7 @@ import Template from "./Template";
 export class Playground extends LitElement {
   @property() public template: any = Template;
 
-  public static styles = [GlobalStyle, Style];
+  public static styles = [Style];
 
   public render() {
     return this.template.bind(this)();
