c84754dd470c95e10b5b584cb77bc215f49c51ac
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-08 12:26:55 -0400

    Chore: Save

--- src/components/PageComponents/Component.ts
+++ src/components/PageComponents/Component.ts
@@ -5,41 +5,17 @@ import GlobalStyle from "../../GlobalStyle";
 import Style from "./PageComponentsStyle";
 import Template from "./PageComponentsTemplate";
 import { components } from "../../Components";
-import { renderForm } from "../Form/Form";
 import { unsafeHTML } from "lit-html/directives/unsafe-html";
 
 @customElement("page-components")
 export class PageComponents extends BeforeRender(LitElement) {
   @property() public component: string;
-  @property() public components: unknown;
+  @property() public components = components;
 
   public static styles = [GlobalStyle, Style];
 
   public async beforeRender() {
     if (!this.component) return;
-    components.map((component: any) => {
-      if (component.tag === this.component) component.src();
-    });
-  }
-
-  get template() {
-    const element: any = document.createElement(this.component);
-    console.log(element);
-    const properties = element.constructor.properties;
-    console.log(properties);
-    return html`
-      ${renderForm({
-        onChange: (_property: string, value: any) =>
-          (element[_property] = value),
-        structure: properties
-      })}
-      ${unsafeHTML(`<${this.component}></${this.component}>`)}
-    `;
-  }
-
-  public connectedCallback() {
-    super.connectedCallback();
-    this.components = components;
     components.map((component: any) => {
       if (component.tag === this.component) component.src();
     });
