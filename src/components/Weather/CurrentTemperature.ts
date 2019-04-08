560aa514fb98ce62e2cc034093794099b4283327
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-07 20:26:54 -0400

    Chore: Save

--- src/components/Weather/CurrentTemperature.ts
+++ src/components/Weather/CurrentTemperature.ts
@@ -1,5 +1,10 @@
 import { html } from "lit-element";
+import { until } from "lit-html/directives/until";
+import { getPeriod } from "./Component";
+import { periodTemplate } from "./PeriodTemplate";
 
 export const currentTemperature = (coordinates: any) => {
-  return html``;
+  return html`
+    ${until(getPeriod(coordinates, 0).then(periodTemplate))}
+  `;
 };
