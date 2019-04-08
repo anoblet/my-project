bc138bb268a8e2d903fab58e00e015e6cbe668b3
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-07 21:11:46 -0400

    Chore: Save

---
+++ src/components/Weather/Templates.ts
@@ -0,0 +1,66 @@
+import { html } from "lit-element";
+import { until } from "lit-html/directives/until";
+import { getPeriod } from "./Component";
+
+export const currentTemperature = (coordinates: any) => {
+  return html`
+    <card-component style="display: flex; flex: 1;">
+      <div slot="title">Temperature</div>
+      <div
+        slot="content"
+        style="display: flex; align-items: center; justify-content: center;"
+      >
+        ${until(
+          getPeriod(coordinates, 0).then(
+            (period: any) =>
+              html`
+                ${period.temperature}&#176; ${period.temperatureUnit}
+              `
+          )
+        )}
+      </div>
+    </card-component>
+  `;
+};
+
+export const shortForecast = (coordinates: any) => {
+  return html`
+    <card-component style="display: flex; flex: 1;">
+      <div slot="title">Short forecast</div>
+      <div
+        slot="content"
+        style="display: flex; align-items: center; justify-content: center;"
+      >
+        ${until(
+          getPeriod(coordinates, 0).then(
+            (period: any) =>
+              html`
+                ${period.shortForecast}
+              `
+          )
+        )}
+      </div>
+    </card-component>
+  `;
+};
+
+export const detailedForecast = (coordinates: any) => {
+  return html`
+    <card-component style="display: flex; flex: 1;">
+      <div slot="title">Short forecast</div>
+      <div
+        slot="content"
+        style="display: flex; align-items: center; justify-content: center;"
+      >
+        ${until(
+          getPeriod(coordinates, 0).then(
+            (period: any) =>
+              html`
+                ${period.shortForecast}
+              `
+          )
+        )}
+      </div>
+    </card-component>
+  `;
+};
