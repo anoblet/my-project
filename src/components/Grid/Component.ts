1ce96f4421ff632e82e2e304c64d53c90b9d2be5
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-08 07:11:12 -0400

    Chore: Save

--- src/components/Grid/Component.ts
+++ src/components/Grid/Component.ts
@@ -5,9 +5,8 @@ import Style from "./Style";
 
 @customElement("grid-component")
 export class GridComponent extends LitElement {
-  static get styles() {
-    return Style;
-  }
+  public static styles = [Style];
+
   public render() {
     return Template.bind(this)();
   }
