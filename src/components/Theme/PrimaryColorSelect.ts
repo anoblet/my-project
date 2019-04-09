1a9064d40168e603e27cffd24c8b0a923fc1d912
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-09 06:25:54 -0400

    Chore: Save

--- src/components/Theme/PrimaryColorSelect.ts
+++ src/components/Theme/PrimaryColorSelect.ts
@@ -1,7 +1,21 @@
 import { html } from "lit-element";
+import { store } from "../../Store";
+import { setState } from "../../State";
+import { convert, setTheme } from "../../Theme";
+
+const updateStore = (e: any) => {
+  const primaryColor = e.target.options[e.target.selectedIndex].value;
+  setTheme(convert({ primaryColor }), document.querySelector("app-component"));
+  setState({
+    type: "app",
+    data: { settings: { theme: { primaryColor } } },
+    store
+  });
+};
 
 export const primaryColorSelect = html`
-  <select
-    ><option>test</option></select
+  <select @input=${updateStore}
+    ><option value="white">White</option
+    ><option value="red">Red</option></select
   >
 `;
