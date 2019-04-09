a24a3725219c707f4e1a46b169af4fdfc766eb6d
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-09 08:22:40 -0400

    Chore: Save

--- src/components/Census/Census.ts
+++ src/components/Census/Census.ts
@@ -3,16 +3,16 @@ import census from "citysdk";
 
 export const getFIPS = ({ latitude, longitude }) => {
   // @todo Fix browser/node irregularities
-  // census(
-  //   {
-  //     vintage: 2015,
-  //     geoHierarchy: {
-  //       county: {
-  //         lat: latitude,
-  //         lng: longitude
-  //       }
-  //     }
-  //   },
-  //   (err: any, res: any) => console.log(res, err)
-  // );
+  census(
+    {
+      vintage: 2015,
+      geoHierarchy: {
+        county: {
+          lat: latitude,
+          lng: longitude
+        }
+      }
+    },
+    (err: any, res: any) => console.log(res, err)
+  );
 };
