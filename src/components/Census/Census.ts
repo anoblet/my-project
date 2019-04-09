f8f3a2a903a1c8c0b30b246b37b2b5a952142334
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-09 10:34:30 -0400

    Chore: Save

--- src/components/Census/Census.ts
+++ src/components/Census/Census.ts
@@ -1,48 +1,59 @@
-// @ts-ignore
 import census from "citysdk";
 
-export const getFIPS = ({ latitude, longitude }) => {
-  // @todo Fix browser/node irregularities
-  census(
-    {
-      vintage: 2015,
-      geoHierarchy: {
-        county: {
-          lat: latitude,
-          lng: longitude
-        }
-      }
-    },
-    (err: any, res: any) => console.log(res, err)
-  );
-  populationByState();
+const censusAsync = async (options: any) => {
+  return new Promise((resolve, reject) => {
+    census(options, (error, result) => {
+      result ? resolve(result) : reject(error);
+    });
+  });
 };
 
-export const populationByState = () => {
-  const states = [];
-  return new Promise((resolve, reject) => {
-    census(
-      {
-        vintage: "2017",
-        geoHierarchy: {
-          state: "*"
-        },
-        sourcePath: ["acs", "acs5"],
-        values: ["B00001_001E"],
-        geoResolution: "500k"
-      },
-      (error, result) => {
-        result.features.map((feature: any) => {
-          const properties = feature.properties;
-          const state = {
-            name: properties.NAME,
-            population: properties["B00001_001E"]
-          };
-          states.push(state);
-        });
-        states.sort((a, b) => (a.name > b.name ? 1 : -1));
-        resolve(states);
+export const getFIPS = ({ latitude, longitude }) => {
+  censusAsync({
+    vintage: 2015,
+    geoHierarchy: {
+      county: {
+        lat: latitude,
+        lng: longitude
       }
-    );
+    }
   });
+};
+
+export const populationByState = async () => {
+  const states = [];
+  const result: any = await censusAsync({
+    vintage: "2017",
+    geoHierarchy: {
+      state: "*"
+    },
+    sourcePath: ["acs", "acs5"],
+    values: ["B00001_001E"],
+    geoResolution: "500k"
+  });
+
+  result.features.map((feature: any) => {
+    const properties = feature.properties;
+    const state = {
+      name: properties.NAME,
+      population: properties["B00001_001E"]
+    };
+    states.push(state);
+  });
+  states.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  return states;
+};
+
+const populationByCountry = async () => {
+  const result: any = await censusAsync({
+    vintage: "2017",
+    geoHierarchy: {
+      state: "0"
+    },
+    sourcePath: ["acs", "acs5"],
+    values: ["B00001_001E"],
+    geoResolution: "500k"
+  });
+  console.log(result);
 };
