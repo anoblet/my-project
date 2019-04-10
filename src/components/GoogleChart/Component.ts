84dfbb233fb781fb618d41b3b0b37297884d5cc0
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-10 16:02:14 -0400

    Chore: Save

--- src/components/GoogleChart/Component.ts
+++ src/components/GoogleChart/Component.ts
@@ -1,8 +1,9 @@
-import { LitElement, css, customElement } from "lit-element";
+import { LitElement, customElement } from "lit-element";
 
 import { GoogleCharts } from "google-charts";
 import Template from "./Template";
 import { properties } from "./Properties";
+import { store } from "../../Store";
 import { style } from "./Style";
 
 @customElement("google-chart")
@@ -10,9 +11,20 @@ export class GoogleChart extends LitElement {
   public static styles = [style];
   public static properties = properties;
   public template = Template;
+  public options = {};
+  public data = [];
+  public type = "bar";
 
-  public constructor() {
-    super();
+  public firstUpdated() {
+    const state = store.getState();
+    const theme = state.app.settings.theme;
+    GoogleCharts.load(() => {
+      const data = GoogleCharts.api.visualization.arrayToDataTable(this.data);
+      const chart = new GoogleCharts.api.visualization.ColumnChart(
+        this.shadowRoot.getElementById("chart")
+      );
+      chart.draw(data, this.options);
+    });
   }
 
   public render() {
--- src/components/GoogleChart/Template.ts
+++ src/components/GoogleChart/Template.ts
@@ -4,5 +4,6 @@ import { renderForm } from "../Form/Form";
 export default function() {
   return html`
     ${renderForm({ structure: this.constructor.properties, values: this })}
+    <div id="chart"></div>
   `;
 }
