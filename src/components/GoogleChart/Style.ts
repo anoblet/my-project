e27552853028df6a4c07eb59635daa9b9bb583a1
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-11 17:49:50 -0400

    Chore: Save

--- src/components/GoogleChart/Style.ts
+++ src/components/GoogleChart/Style.ts
@@ -2,12 +2,7 @@ import { css } from "lit-element";
 
 export const style = css`
   :host {
-    display: flex;
     flex: 1;
-    position: absolute;
-    left: 0;
-    right: 0;
-    max-width: 100%;
   }
 
   #chart {
@@ -16,10 +11,6 @@ export const style = css`
 
   .absolute {
     flex: 1;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
   }
 `;
 
