2661f1dfa1eb7f01d636bc0df5969ac916e62c0c
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-12 21:48:04 -0400

    Chore: Save

--- src/components/ChartJS/Component.ts
+++ src/components/ChartJS/Component.ts
@@ -45,14 +45,7 @@ export class ChartJS extends LitElement {
             label: "Population",
             data,
             backgroundColor: "rgba(255, 99, 132, 1)",
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
-            ],
+            borderColor: "rgba(255, 159, 64, 1)",
             borderWidth: 1
           }
         ]
