cd6bc55a4a750168617ae4b825f8e93826d5e44e
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-13 21:01:49 -0400

    Chore: Save

--- src/components/ChartJS/Demo.ts
+++ src/components/ChartJS/Demo.ts
@@ -1,8 +1,26 @@
 import { LitElement, customElement, html } from "lit-element";
 
+import { populationByState } from "../CitySDK/CitySDK";
+import { until } from "lit-html/directives/until";
+
 @customElement("demo-component")
 export class Demo extends LitElement {
   public render() {
-    return html``;
+    return html`
+      ${until(
+        populationByState().then((data: any) => {
+          const labels = [];
+          const values = [];
+          data.map((state: any) => {
+            labels.push(state.name);
+            values.push(state.population);
+          });
+          const _data =  { labels, values };
+          return html`
+            <chart-js .data=${_data}></chart-js>
+          `;
+        })
+      )})
+    `;
   }
 }
