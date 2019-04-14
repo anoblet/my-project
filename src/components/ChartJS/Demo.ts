d3a8d6662796e4009d6d4386f0dd979a70909757
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-13 20:13:39 -0400

    Chore: Save

---
+++ src/components/ChartJS/Demo.ts
@@ -0,0 +1,8 @@
+import { LitElement, customElement, html } from "lit-element";
+
+@customElement("demo-component")
+export class Demo extends LitElement {
+  public render() {
+    return html``;
+  }
+}
