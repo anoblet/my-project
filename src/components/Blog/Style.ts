a461d2431520efa3f3ac7b3edfd0e67da18c7783
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-16 07:05:02 -0400

    Chore: Save

--- src/components/Blog/Style.ts
+++ src/components/Blog/Style.ts
@@ -1,16 +1,3 @@
 import { css } from "lit-element";
 
-export default css`
-  * {
-    box-sizing: border-box;
-  }
-
-  :host {
-    flex: 1;
-    position: relative;
-  }
-
-  grid-component {
-    grid-gap: var(--grid-gap);
-  }
-`;
+export default css``;
