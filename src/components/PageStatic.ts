c0a50dbee963c76042f95724dfc2d0f7b180a6af
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-17 06:45:19 -0400

    Chore: Save

--- src/components/PageStatic.ts
+++ src/components/PageStatic.ts
@@ -103,7 +103,7 @@ export class PageStatic extends LitElement {
                 <a href="https://open-wc.org/">Open WC</a>
               </card-component>
               <card-component title="Heroes">
-                The starlight(lit*) team
+                The star-light(lit*) team
                 @justin @bennyp @dakMoR @lars @westbrook @john @captaincodeman
               </card-component>
               <card-component title="Patreon">
