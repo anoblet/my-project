1dbc01e01a71b5b42f8d1f44891b3a8aabcca02f
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-19 16:34:28 -0400

    Chore: Save

--- src/components/User/Template.ts
+++ src/components/User/Template.ts
@@ -1,14 +1,13 @@
-import { isSignedIn, signOut } from "../../User";
-
-import { getForm } from "./Component";
 import { html } from "lit-element";
+import { isSignedIn, signOut } from "../../User";
 import { until } from "lit-html/directives/until";
+import { getForm } from "./Component";
 
 export default () => {
   return html`
-    <card-component>
+    <card-component grow>
       <div slot="content">
-        ${isSignedIn()
+        ${false && isSignedIn()
           ? html`
               <mwc-button raised @click="${signOut}">Sign out</mwc-button>
             `
