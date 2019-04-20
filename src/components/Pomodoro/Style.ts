a977fa0e8abdab469313974c7a5c31fade862dc2
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-20 13:01:56 -0400

    Chore: Save

--- src/components/Pomodoro/Style.ts
+++ src/components/Pomodoro/Style.ts
@@ -1,14 +1,3 @@
 import { css } from "lit-element";
 
-export default css`
-  button {
-    display: flex;
-    flex: 1;
-    justify-content: center;
-    background: inherit;
-    border: 1px solid var(--primary-color);
-    padding: var(--button-padding, 1em);
-    color: var(--button-color);
-    border-radius: var(--border-radius);
-  }
-`;
+export default css``;
