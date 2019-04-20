1fc01ffcea81e3b9721904fadfb6f6f98efbacb3
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-20 11:42:04 -0400

    Chore: Save

--- src/components/Header/Template.ts
+++ src/components/Header/Template.ts
@@ -1,9 +1,7 @@
 import { html } from "lit-element";
-import { primaryColorSelect } from "../Theme/PrimaryColorSelect";
 
 export default function() {
   return html`
     <slot></slot>
-    <!-- <slot name="choose-theme"></slot> -->
   `;
 }
