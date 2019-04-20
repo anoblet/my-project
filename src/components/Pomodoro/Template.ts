f86141f71733380f08591ee6ee23f2e2e53fda09
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-20 13:01:43 -0400

    Chore: Save

--- src/components/Pomodoro/Template.ts
+++ src/components/Pomodoro/Template.ts
@@ -1,14 +1,5 @@
 import { html } from "lit-element";
 
 export default function() {
-  switch (this.theme) {
-    case "material":
-      return html`
-        <mwc-button label=${this.label}></mwc-button>
-      `;
-    default:
-      return html`
-        <button>${this.label}<slot></slot></button>
-      `;
-  }
+  return html``;
 }
