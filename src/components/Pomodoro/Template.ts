0789e5827a7cbe7468958eef3c8683f1821faaca
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-20 19:35:53 -0400

    Chore: Save

--- src/components/Pomodoro/Template.ts
+++ src/components/Pomodoro/Template.ts
@@ -2,16 +2,14 @@ import { html } from "lit-element";
 
 export default function() {
   return html`
-    <grid-component>
-      <div>
-        <grid-component id="input" columns="2">
-          <input name="minutes" type="text" value="20" />
-          <input name="seconds" type="text" value="00" />
-        </grid-component>
-      </div>
-      <div>
-        <button-component>Start</button-component>
-      </div>
-    </grid-component>
+    <div>
+      <grid-component id="input" columns="2">
+        <input name="minutes" type="text" value="20" />
+        <input name="seconds" type="text" value="00" />
+      </grid-component>
+    </div>
+    <div>
+      <button-component>Start</button-component>
+    </div>
   `;
 }
