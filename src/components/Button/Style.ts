1c83766c14c5a7d3f1425e9e32bf22f950047dd6
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-21 04:09:33 -0400

    Chore: Save

--- src/components/Button/Style.ts
+++ src/components/Button/Style.ts
@@ -6,7 +6,7 @@ export default css`
     flex: 1;
     justify-content: center;
     background: inherit;
-    border: 1px solid var(--primary-color);
+    border: 1px solid var(--border-color);
     padding: var(--button-padding, 1em);
     color: var(--button-color);
     border-radius: var(--border-radius);
