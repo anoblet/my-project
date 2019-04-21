c37536ace409c2bd160a6c390cc4a3538f2b2a8e
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-21 14:21:25 -0400

    Chore: Save

--- src/components/Pomodoro/Component.ts
+++ src/components/Pomodoro/Component.ts
@@ -10,21 +10,21 @@ export class Component extends LitElement {
   public template = Template;
   public render = this.template.bind(this);
 
-  protected timeleft = 1200;
+  protected _timeleft = 1200;
   protected _interval: any;
   protected _currentStep: number = 0;
   protected _currentMode: string = "focus";
 
   public get _minutes() {
-    return Math.floor(this.timeleft / 60);
+    return Math.floor(this._timeleft / 60);
   }
 
   public get _seconds() {
-    return this.timeleft - this._minutes * 60;
+    return this._timeleft - this._minutes * 60;
   }
 
   public start() {
-    this._interval = setInterval(() => this.timeleft--, 1000);
+    this._interval = setInterval(() => this._timeleft--, 1000);
   }
 
   public stop() {
@@ -34,7 +34,7 @@ export class Component extends LitElement {
 
   public reset() {
     this.stop();
-    this.timeleft = 1200;
+    this._timeleft = 1200;
   }
 
   public selectStep(step: number) {
