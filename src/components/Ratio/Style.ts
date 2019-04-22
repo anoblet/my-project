ce98b1edb943ae9a10c9438522c49ec18b311e11
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-22 11:46:24 -0400

    Chore: Save

--- src/components/Ratio/Style.ts
+++ src/components/Ratio/Style.ts
@@ -1,4 +1,9 @@
 import { css } from "lit-element";
 
 export default css`
+  :host {
+    display: flex;
+    flex: 1;
+    border: 1px solid #000;
+  }
 `;
--- src/components/Ratio/Template.ts
+++ src/components/Ratio/Template.ts
@@ -1,5 +1,5 @@
 import { html } from "lit-element";
 
 export default function() {
-  return html``;
+  return html`test`;
 }
