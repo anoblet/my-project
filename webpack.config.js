91ef909e2d71a27f1fc066cad20e0598ce7239f4
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-24 12:12:58 -0400

    Chore: Save

--- webpack.config.js
+++ webpack.config.js
@@ -59,18 +59,6 @@ module.exports = {
         parallel: true,
         sourceMap: true,
         terserOptions: {
-          ecma: undefined,
-          warnings: false,
-          parse: {},
-          compress: {},
-          mangle: true, // Note `mangle.properties` is `false` by default.
-          module: false,
-          output: null,
-          toplevel: false,
-          nameCache: null,
-          ie8: false,
-          keep_classnames: undefined,
-          keep_fnames: false,
           safari10: true
         }
       })
