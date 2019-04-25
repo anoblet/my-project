e542e053460ab5b268eaa993d7741cf9b04a226d
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-25 10:40:26 -0400

    Chore: Save

--- src/components/Ratio/Component.ts
+++ src/components/Ratio/Component.ts
@@ -11,24 +11,29 @@ export class RatioComponent extends LitElement {
   public render = this.template.bind(this);
 
   @property() public ratio: number = 1;
+  @property() public width: number = 1;
 
   public connectedCallback() {
     super.connectedCallback();
     this.observeResize();
   }
 
+  public updated(changedProperties) {
+    if (changedProperties.has("ratio")) this.onResize(this.width);
+  }
+
   public observeResize() {
     const resizeObserver = new ResizeObserver((entries: any) => {
       for (const entry of entries) {
-        const width = entry.contentRect.width;
-        const height = width * this.ratio;
-        this.style.setProperty("height", height + "px");
+        this.width = entry.contentRect.width;
+        this.onResize(this.width);
       }
     });
     resizeObserver.observe(this);
   }
 
-  public onResize(entry) {
-    return;
+  public onResize(width: number) {
+    const height = width * this.ratio;
+    this.style.setProperty("height", height + "px");
   }
 }
