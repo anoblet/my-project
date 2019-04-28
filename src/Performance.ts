52ef8714e734ec5df23ecfa6b4ddb4f6dc721bb8
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-28 00:51:39 -0400

    Chore: Save

--- src/Performance.ts
+++ src/Performance.ts
@@ -1,10 +1,6 @@
 import { firebase } from "./Firebase";
 // Opinion
 import { config } from "../config";
-
-// Store as a singleton (if needed an iife)
-// const performance ;
-
 /**
  * Assume performance returns the performance object no matter the state of the app
  * @return firebase.performance
