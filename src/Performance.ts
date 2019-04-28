36c39e3f6f16cd581faaef771d246ca82b9a0691
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-28 00:18:32 -0400

    Chore: Save

--- src/Performance.ts
+++ src/Performance.ts
@@ -2,15 +2,19 @@ import { firebase } from "./Firebase";
 // Opinion
 import { config } from "../config";
 
+// Store as a singleton (if needed an iife)
+// const performance ;
+
 export const performance = async () => {
   const _instance = await firebase.init(config.firebase);
   return _instance.performance();
 };
 
 export const trace = () => {
+  // Kina weird but instance needs an instance of performance
   return;
 };
 
-const start = () => {
+const start = (label: string) => {
   return;
 };
