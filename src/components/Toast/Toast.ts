1997175923a307f5c027ce3b7c761d2d72c7da14
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-28 00:02:03 -0400

    Chore: Save

--- src/components/Toast/Toast.ts
+++ src/components/Toast/Toast.ts
@@ -6,12 +6,12 @@ import "./ToastComponent";
  *
  * @todo Separate the helper from the ui
  */
-export const toast = (message: string) => {
+export const toast = (message: string, config: { timeout: 2500 }) => {
   const element: any = document.createElement("toast-component");
   element.content = message;
   const container = document
     .querySelector("app-component")
     .shadowRoot.querySelector("#content");
   container.appendChild(element);
-  setTimeout(() => element.parentNode.removeChild(element), 2500);
+  setTimeout(() => element.parentNode.removeChild(element), config.timeout);
 };
