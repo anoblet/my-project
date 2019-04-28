d65d94b6579cd28d8e31fdd1b5801fb9c86bf283
Andrew Noblet <andrewbnoblet@gmail.com>
2019-04-28 00:37:29 -0400

    Chore: Save

--- src/Performance.ts
+++ src/Performance.ts
@@ -5,6 +5,10 @@ import { config } from "../config";
 // Store as a singleton (if needed an iife)
 // const performance ;
 
+/**
+ * Assume performance returns the performance object no matter the state of the app
+ * @return firebase.performance
+ */
 export const performance = async () => {
   const _instance = await firebase.init(config.firebase);
   return _instance.performance();
