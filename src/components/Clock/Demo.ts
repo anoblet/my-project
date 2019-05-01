a9e39ffc30de06478e9526bf02b8b70ed62a9839
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-01 02:56:59 -0400

    Chore: Save

--- src/components/Clock/Demo.ts
+++ src/components/Clock/Demo.ts
@@ -12,11 +12,15 @@ export class Demo extends LitElement {
         left: 0;
         right: 0;
         bottom: 0;
+        display: flex;
+        flex: 1;
       }
     `
   ];
 
   public render() {
-    return html``;
+    return html`
+      <clock-component></clock-component>
+    `;
   }
 }
