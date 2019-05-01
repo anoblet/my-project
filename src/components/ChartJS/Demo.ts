1189a9ea5445fe129299f3cc0afcb48b3ecd6282
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-01 09:01:09 -0400

    Chore: Save

--- src/components/ChartJS/Demo.ts
+++ src/components/ChartJS/Demo.ts
@@ -83,9 +83,7 @@ export class Demo extends BeforeRender(LitElement) {
             labels.map(() => colors.push(MaterialColor.getColor()));
             const data = { data: values, labels, backgroundColor: colors };
             return html`
-              <ratio-component ratio="1">
-                <chart-js .data=${data} .type=${this.type}></chart-js>
-              </ratio-component>
+              <chart-js .data=${data} .type=${this.type}></chart-js>
             `;
           }),
           html`
