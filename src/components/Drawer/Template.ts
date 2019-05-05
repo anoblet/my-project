36c70d658dd1f69a3b5ef6cf948ca62bb7d5391a
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-05 08:41:05 -0400

    Chore: Save

--- src/components/Drawer/Template.ts
+++ src/components/Drawer/Template.ts
@@ -1,8 +1,8 @@
 import { html } from "lit-element";
-// import { navigation } from "../Layout/Template";
+import { navigation } from "../Layout/Template";
 
 export default function() {
   return html`
-    <slot></slot>
+    ${navigation.bind(this)()}
   `;
 }
