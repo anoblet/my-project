0aba2769b871fcf96330646af3ef8ca5be7d2479
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-08 16:18:04 -0400

    Chore: Save

--- src/Icons.ts
+++ src/Icons.ts
@@ -39,3 +39,27 @@ export const mail_outline = html`
     />
   </svg>
 `;
+
+export const expand_more = html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+`;
+
+export const expand_less = html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+`;
