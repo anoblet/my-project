18b89ab740f2c95c36d54c3313e66e2309a5c156
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-08 16:16:23 -0400

    Chore: Save

--- src/Icons.ts
+++ src/Icons.ts
@@ -11,3 +11,31 @@ export const home = html`
     <path d="M0 0h24v24H0z" fill="none" />
   </svg>
 `;
+
+export const code = html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path fill="none" d="M0 0h24v24H0V0z" />
+    <path
+      d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"
+    />
+  </svg>
+`;
+
+export const mail_outline = html`
+  <svg
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path fill="none" d="M0 0h24v24H0z" />
+    <path
+      d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"
+    />
+  </svg>
+`;
--- src/components/Footer/Template.ts
+++ src/components/Footer/Template.ts
@@ -1,4 +1,5 @@
 import { html } from "lit-element";
+import { code, home, mail_outline } from "../../Icons";
 
 export default function() {
   return html`
@@ -7,14 +8,19 @@ export default function() {
       rel="noreferrer"
       target="_blank"
       class="item"
+      @click="${this._toggleDrawer}"
     >
-      <i class="material-icons" @click="${this._toggleDrawer}">code</i>
+      ${code}
     </a>
-    <a id="home" href="/" class="item">
-      <i class="material-icons" @click="${this._toggleDrawer}">home</i>
+    <a id="home" href="/" class="item" @click="${this._toggleDrawer}">
+      ${home}
     </a>
-    <a href="mailto: andrewbnoblet@gmail.com" class="item">
-      <i class="material-icons" @click="${this._toggleDrawer}">mail_outline</i>
+    <a
+      href="mailto: andrewbnoblet@gmail.com"
+      class="item"
+      @click="${this._toggleDrawer}"
+    >
+      ${mail_outline}
     </a>
   `;
 }
