bd79d12c4a1f96dfb4bcd1a5996d69c994d5b313
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-13 09:30:11 -0400

    Chore: Save

--- src/components/Poll/Template.ts
+++ src/components/Poll/Template.ts
@@ -2,13 +2,16 @@ import { html } from "lit-element";
 
 export default function() {
   return html`
-    <button-component label="Create" outlined @click=${this.createDialog}></button-component>
-    <div id="create-dialog" hidden>
-      ${createDialog}
-    </div>
+    <button-component
+      label="Create"
+      outlined
+      @click=${this.createDialog}
+    ></button-component>
+    <dialog-component>${createDialog}</dialog-component>
+
   `;
 }
 
 const createDialog = html`
-  <card-component title="Create">Test</card-component>
+  Test
 `;
