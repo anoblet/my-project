15753b5334acf7131d514ff69833b28d9c0ac44e
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-13 07:01:36 -0400

    Chore: Save

--- src/components/Dialog/Demo.ts
+++ src/components/Dialog/Demo.ts
@@ -1,9 +1,9 @@
-import "../Poll/Component";
-import GlobalStyle from "../../GlobalStyle";
+import "./Component";
 
-import { LitElement, css, customElement, html} from "lit-element";
+import { LitElement, css, customElement, html } from "lit-element";
 
 import { BeforeRender } from "../../mixins/BeforeRender";
+import GlobalStyle from "../../GlobalStyle";
 
 @customElement("dialog-component-demo")
 export class Demo extends BeforeRender(LitElement) {
