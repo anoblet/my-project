3b225e83b605cd340fdb32459e7bdc2ef8a37f2a
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-19 13:07:45 -0400

    Chore: Save

--- src/components/Voting/View.ts
+++ src/components/Voting/View.ts
@@ -1,3 +1,20 @@
-import { html } from "lit-element";
+import { LitElement, css, customElement, html, property } from "lit-element";
 
-export const view = html`Hi`;
+export const view = () =>
+  html`
+    Hi
+  `;
+
+/**
+ * Voting component
+ */
+@customElement("view-poll")
+export class Component extends LitElement {
+  public static styles = css``;
+
+  @property() pollId: string;
+
+  public render() {
+    return html`Hi`;
+  }
+}
