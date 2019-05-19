6ad81395ac3f5c1c95e4d14dc7132cbabdea8b16
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-19 14:50:57 -0400

    Chore: Save

--- src/components/Voting/View.ts
+++ src/components/Voting/View.ts
@@ -1,4 +1,5 @@
 import { LitElement, css, customElement, html, property } from "lit-element";
+import Firebase from "../../Firebase";
 
 /**
  * Voting component
@@ -7,11 +8,27 @@ import { LitElement, css, customElement, html, property } from "lit-element";
 export class Component extends LitElement {
   public static styles = css``;
 
+  @property() data: { title; options };
   @property() pollId: string;
+
+  connectedCallback() {
+    super.connectedCallback();
+    Firebase.getDocument({
+      path: `polls/${this.pollId}`,
+      callback: data => (this.data = data),
+      watch: true
+    });
+  }
 
   public render() {
     return html`
-      ${this.pollId}
+      <h2>${this.data.title}</h2>
+      ${this.data.options.map(
+        option =>
+          html`
+            ${option}
+          `
+      )}
     `;
   }
 }
