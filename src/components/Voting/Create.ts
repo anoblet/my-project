b8ee054f9ecf0b5303f6584a3afd12a72a6a5bac
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-26 12:48:35 -0400

    Chore: Save

--- src/components/Voting/Create.ts
+++ src/components/Voting/Create.ts
@@ -4,56 +4,69 @@ import { delete_outline } from "../../Icons";
 
 const Template = function() {
   return html`
-    <grid-component>
-      <form id="form-create">
-        <grid-component>
-          <grid-component id="title-container">
-            <label for="title">Title</label
-            ><input
-              id="title"
-              type="text"
-              name="title"
-              .value=${this.title}
-              @input=${(e: any) => (this.title = e.target.value)}
-            />
-          </grid-component>
-          <div id="actions">
-            <button-component
-              label="Add item"
-              @click=${this.addOption}
-            ></button-component>
-            <button-component
-              label="Save"
-              @click=${this.save}
-            ></button-component>
-          </div>
-          <grid-component id="option-container">
-            ${this.options.map(
-              (option: any, index: number) =>
-                html`
-                  <label>#${index}</label>
-                  <input
-                    type="text"
-                    .value=${option}
-                    @input=${(e: any) => (this.options[index] = e.target.value)}
-                  />
-                  <span class="icon" @click=${() => this.removeOption(index)}
-                    >${delete_outline}</span
-                  >
-                `
-            )}
-          </grid-component>
+    <form id="form-create">
+      <grid-component>
+        <grid-component id="title-container">
+          <label for="title">Title</label
+          ><input
+            id="title"
+            type="text"
+            name="title"
+            .value=${this.title}
+            @input=${(e: any) => (this.title = e.target.value)}
+          />
         </grid-component>
-      </form>
-      <card-component title="Form data">
-        Title: ${JSON.stringify(this.title)} Items:
-        ${JSON.stringify(this.items)}
-      </card-component>
-    </grid-component>
+        <div id="actions">
+          <button-component
+            label="Add option"
+            @click=${this.addOption}
+          ></button-component>
+          <button-component label="Save" @click=${this.save}></button-component>
+        </div>
+        <grid-component id="option-container">
+          ${this.options.map(
+            (option: any, index: number) =>
+              html`
+                <label>#${index}</label>
+                <input
+                  type="text"
+                  .value=${option}
+                  @input=${(e: any) => (this.options[index] = e.target.value)}
+                />
+                <span class="icon" @click=${() => this.removeOption(index)}
+                  >${delete_outline}</span
+                >
+              `
+          )}
+        </grid-component>
+      </grid-component>
+    </form>
   `;
 };
 
-const Style = css``;
+const Style = css`
+  #actions {
+    display: grid;
+    grid-template-columns: max-content max-content;
+    grid-gap: 1em;
+    justify-content: flex-end;
+  }
+
+  #option-container {
+    display: grid;
+    grid-template-columns: max-content auto max-content;
+    grid-gap: 1em;
+  }
+
+  label {
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    fill: currentColor;
+  }
+`;
 
 @customElement("poll-create")
 export class Component extends LitElement {
