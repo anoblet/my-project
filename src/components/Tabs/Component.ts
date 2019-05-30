2cc289cca55cd4dc9913e1b24d7f2403e1dfe184
Andrew Noblet <andrewbnoblet@gmail.com>
2019-05-30 06:53:46 -0400

    Chore: Save

---
+++ src/components/Tabs/Component.ts
@@ -0,0 +1,13 @@
+import { LitElement, customElement, property } from "lit-element";
+
+import Style from "./Style";
+import Template from "./Template";
+
+/**
+ * Icon
+ */
+@customElement("component-tabs")
+export class Tabs extends LitElement {
+  public static styles = [Style];
+  public render = Template.bind(this);
+}
