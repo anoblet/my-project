aaef820a46a4174a576c338964b994a2cc4b137f
Andrew Noblet <andrewbnoblet@gmail.com>
2019-06-06 19:36:03 -0400

    Chore: Save

--- src/components/Almanac/Demo.ts
+++ src/components/Almanac/Demo.ts
@@ -4,8 +4,6 @@ import { LitElement, css, customElement, html } from "lit-element";
 
 @customElement("component-almanac-demo")
 export class Demo extends LitElement {
-  public static styles = [css``];
-
   public render() {
     return html`
       <component-almanac></component-almanac>
