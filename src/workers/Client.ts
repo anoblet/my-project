7a99dd6f9d3ad7946d797fc214155f6e1fb4429f
Andrew Noblet <andrewbnoblet@gmail.com>
2019-06-24 10:44:21 -0400

    Chore: Save

--- src/workers/Client.ts
+++
@@ -1,8 +0,0 @@
-import * as Comlink from "comlink";
-
-const MyClass: any = Comlink.wrap(new Worker("/js/workers/Firebase.ts"));
-
-(async () => {
-  const instance: any = await new MyClass();
-  await instance.logSomething();
-})();
