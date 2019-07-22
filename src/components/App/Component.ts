418fab09c7dd9b91aafb00a180b577ec3e4d867e
Andrew Noblet <andrewbnoblet@gmail.com>
2019-07-22 09:23:34 -0400

    Chore: Save

--- src/components/App/Component.ts
+++ src/components/App/Component.ts
@@ -72,10 +72,8 @@ export class App extends BeforeRenderMixin(LitElement) {
         const footer: LitElement = this.shadowRoot.querySelector("#footer");
         if (prevPosition > currentPosition) {
           footer.removeAttribute("hidden");
-          footer.style.display = "block";
         } else {
           footer.setAttribute("hidden", "");
-          footer.style.display = "none";
         }
         prevPosition = currentPosition;
       };
