facf52f38094549ad3e40286b6592bb96002a573
Andrew Noblet <andrewbnoblet@gmail.com>
2019-08-05 18:47:04 -0400

    Chore: Save

--- src/components/App/Template.ts
+++ src/components/App/Template.ts
@@ -2,6 +2,7 @@ import { html } from "lit-element";
 import header from "./templates/Header";
 import drawer from "./templates/Drawer";
 import footer from "./templates/Footer";
+import "../Breadcrumb/Component";
 
 export default function() {
   return html`
@@ -11,6 +12,7 @@ export default function() {
     <drawer-component
       ><div id="drawer" slot="drawer">${drawer()}</div>
       <div slot="main">
+      <breadcrumb-component></breadcrumb-component>
         <profile-menu id="profile-menu"></profile-menu>
         <div id="portal"></div>
       </div>
