1f8cd6c8a981c1c900a9afa1c47b1057ff791edb
Andrew Noblet <andrewbnoblet@gmail.com>
2019-08-12 14:07:28 -0400

    Chore: Save

--- src/components/Grid/Style.ts
+++
@@ -1,8 +0,0 @@
-import { css } from "lit-element";
-
-export default css`
-  :host {
-    display: grid;
-    grid-gap: var(--grid-gap, 1em);
-  }
-`;
--- src/components/Grid/Template.ts
+++
@@ -1,7 +0,0 @@
-import { html } from "lit-element";
-
-export default function() {
-  return html`
-    <slot></slot>
-  `;
-}
