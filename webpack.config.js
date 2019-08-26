62a729253e53db8f144b793c49a24be659845ad9
Andrew Noblet <andrewbnoblet@gmail.com>
2019-08-26 05:57:05 -0400

    Chore: Save

--- webpack.config.js
+++ webpack.config.js
@@ -27,17 +27,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.md$/,
-        use: [
-          {
-            loader: "html-loader"
-          },
-          {
-            loader: "markdown-loader"
-          }
-        ]
-      },
-      {
         test: /\.tsx?$/,
         use: "ts-loader"
       }
