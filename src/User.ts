015e98b298ee4a8ff3dd834e1808d610d43df192
Andrew Noblet <andrewbnoblet@gmail.com>
2019-08-27 10:53:47 -0400

    Chore: Save

--- src/User.ts
+++ src/User.ts
@@ -111,7 +111,7 @@ export const getUserTheme = async (callback?: any) => {
  * @return [description]
  */
 export const getTheme = async (uid: string) => {
-  return await new Promise(resolve => {
+  return await new Promise((resolve) => {
     return getDocument({
       path: `users/${uid}/settings/theme`,
       callback: (document: any) => {
@@ -158,10 +158,10 @@ export const get = () => {
 
 export const getIp = async () => {
   return fetch("https://api.ipify.org?format=json")
-    .then(response => {
+    .then((response) => {
       return response.json();
     })
-    .then(data => data.ip);
+    .then((data) => data.ip);
 };
 
 export const user = {
