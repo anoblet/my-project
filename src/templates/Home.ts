ef1648c5dd6c38d416c11232c926c19c7cfec4f2
Andrew Noblet <andrewbnoblet@gmail.com>
2019-09-06 12:26:46 -0400

    Chore: Save

---
+++ src/templates/Home.ts
@@ -0,0 +1,58 @@
+import "./Ratio/Component";
+
+import { create, extension, notes, show_chart } from "@anoblet/material-icons";
+
+import { html } from "lit-element";
+
+export default html`
+  <grid-component id="grid">
+    <div class="grid-item">
+      <a href="/components">
+        <ratio-component>
+          <div class="item">
+            <span class="label"><h1>Components</h1></span>
+            <span class="icon">
+              ${extension}
+            </span>
+          </div>
+        </ratio-component>
+      </a>
+    </div>
+    <div class="grid-item">
+      <a href="/readme">
+        <ratio-component>
+          <div class="item">
+            <span class="label"><h1>Readme</h1></span>
+            <span class="icon">
+              ${notes}
+            </span>
+          </div>
+        </ratio-component>
+      </a>
+    </div>
+    <div class="grid-item">
+      <a href="/blog">
+        <ratio-component>
+          <div class="item">
+            <span class="label"><h1>Blog</h1></span>
+            <span class="icon">
+              ${create}
+            </span>
+          </div></ratio-component
+        ></a
+      >
+    </div>
+    <div class="grid-item">
+      <a href="/Performance">
+        <ratio-component>
+          <div class="item">
+            <span class="label"><h1>Performance</h1></span>
+            <span class="icon">
+              ${show_chart}
+            </span>
+          </div></ratio-component
+        ></a
+      >
+    </div>
+  </grid-component>
+`;
