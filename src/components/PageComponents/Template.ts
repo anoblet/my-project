4019052836181c206ab9399bd35aa6163a2f97b6
Andrew Noblet <andrewbnoblet@gmail.com>
2019-09-07 02:49:29 -0400

    Chore: Save

--- src/components/PageComponents/Template.ts
+++ src/components/PageComponents/Template.ts
@@ -2,24 +2,18 @@ import { html } from "lit-element";
 
 export default function() {
   return html`
-    <grid-component id="content-grid">
-      <card-component title="Components">
-        <div slot="content">
-          <grid-component id="columns">
-            ${this.components.map(
-              (component: any) => html`
-                <a href="/components/${component.tag}">
-                  <card-component>
-                    <div class="center">
-                      ${component.label}
-                    </div>
-                  </card-component></a
-                >
-              `
-            )}
-          </grid-component>
-        </div></card-component
-      >
+    <grid-component id="columns">
+      ${this.components.map(
+        (component: any) => html`
+          <a href="/components/${component.tag}">
+            <card-component>
+              <div class="center">
+                ${component.label}
+              </div>
+            </card-component></a
+          >
+        `
+      )}
     </grid-component>
   `;
 }
