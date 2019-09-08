0a8da4f168c5797f2dff84f4f15f8e58e2b9ca99
Andrew Noblet <andrewbnoblet@gmail.com>
2019-09-08 19:07:49 -0400

    Chore: Save

--- src/templates/MadeWith.ts
+++ src/templates/MadeWith.ts
@@ -1,5 +1,11 @@
 import { html } from "lit-element";
 
 export default function() {
-  return html`Made with &hearts; by Andrew Noblet`;
+  return html`
+    <card-component
+      ><div slot="body">
+        Made with &hearts; by Andrew Noblet
+      </div></card-component
+    >
+  `;
 }
