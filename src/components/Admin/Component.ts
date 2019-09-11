f3a5dd4cec2d9e9ec935262d8417c29aa303797a
Andrew Noblet <andrewbnoblet@gmail.com>
2019-09-11 14:25:24 -0400

    Chore: Save

--- src/components/Admin/Component.ts
+++ src/components/Admin/Component.ts
@@ -7,6 +7,5 @@ import Template from "./Template";
 @customElement("admin-component")
 export class AdminComponent extends LitElement {
   public static styles = [GlobalStyle, Style];
-  public template = Template;
-  public render = this.template.bind(this);
+  public render = Template.bind(this);
 }
