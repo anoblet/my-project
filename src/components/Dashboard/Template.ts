1798f502cb23794621d6f5d3d8df17552e44d14f
Andrew Noblet <andrewbnoblet@gmail.com>
2019-09-26 12:00:24 -0400

    Chore: Save

--- src/components/Dashboard/Template.ts
+++ src/components/Dashboard/Template.ts
@@ -1,12 +1,12 @@
-import { html } from "lit-element";
-import {
-  currentTemperature,
-  detailedForecast,
-  shortForecast,
-  windDirection,
-  windSpeed
-} from "../Weather/Templates";
 import "../Clock/Component";
+
+import { detailedForecast, windSpeed } from "../Weather/Templates";
+
+import { Forecast } from "@anoblet/weather";
+import { html } from "lit-element";
+import { until } from "lit-html/directives/until";
+
+const period = new Forecast("40.7666688", "-73.961472").getPeriod(0);
 
 export default function() {
   return html`
@@ -14,10 +14,8 @@ export default function() {
       <div class="item">
         <div class="item-content tall">
           <card-component>
-            <div
-              slot="body"
-              style="display: flex; align-items: center; justify-content: center;"
-            >
+            <span slot="title">Clock</span>
+            <div class="centered" slot="body">
               <clock-component></clock-component>
             </div>
           </card-component>
@@ -30,19 +28,23 @@ export default function() {
       </div>
       <div class="item">
         <div class="item-content tall">
-          ${currentTemperature({
-            latitude: "40.7666688",
-            longitude: "-73.961472"
-          })}
+          <card-component>
+            <span slot="title">Temperature</span>
+            <div class="centered" slot="body">
+              ${until(period.then(period => period.temperature))}
+            </div>
+          </card-component>
         </div>
       </div>
 
       <div class="item">
         <div class="item-content wide">
-          ${shortForecast({
-            latitude: "40.7666688",
-            longitude: "-73.961472"
-          })}
+          <card-component>
+            <span slot="title">Short forecast</span>
+            <div class="centered" slot="body">
+              ${until(period.then(period => period.shortForecast))}
+            </div>
+          </card-component>
         </div>
       </div>
       <div class="item">
@@ -52,10 +54,12 @@ export default function() {
       </div>
       <div class="item">
         <div class="item-content tall">
-          ${windDirection({
-            latitude: "40.7666688",
-            longitude: "-73.961472"
-          })}
+          <card-component>
+            <span slot="title">Wind direction</span>
+            <div class="centered" slot="body">
+              ${until(period.then(period => period.windDirection))}
+            </div>
+          </card-component>
         </div>
       </div>
       <div class="item">
@@ -65,10 +69,12 @@ export default function() {
       </div>
       <div class="item">
         <div class="item-content wide">
-          ${detailedForecast({
-            latitude: "40.7666688",
-            longitude: "-73.961472"
-          })}
+          <card-component>
+            <span slot="title">Detailed forecast</span>
+            <div class="centered" slot="body">
+              ${until(period.then(period => period.detailedForecast))}
+            </div>
+          </card-component>
         </div>
       </div>
       <div class="item">
@@ -78,10 +84,12 @@ export default function() {
       </div>
       <div class="item">
         <div class="item-content tall">
-          ${windSpeed({
-            latitude: "40.7666688",
-            longitude: "-73.961472"
-          })}
+          <card-component>
+            <span slot="title">Wind speed</span>
+            <div class="centered" slot="body">
+              ${until(period.then(period => period.windSpeed))}
+            </div>
+          </card-component>
         </div>
       </div>
       <div class="item">
