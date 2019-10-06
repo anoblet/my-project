a22b113d6b808d57f2c522086eaadb863b449067
Andrew Noblet <andrewbnoblet@gmail.com>
2019-10-05 20:27:11 -0400

    Chore: Save

--- src/components/App/Component.ts
+++ src/components/App/Component.ts
@@ -19,7 +19,6 @@ import { toast } from "../Toast/Toast";
 
 // Let's assume CSS modules are a thing in the future
 const styleImport = require("./style.css");
-console.log(styleImport);
 const Style = css`
   ${unsafeCSS(styleImport)}
 `;
