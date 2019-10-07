3dcc7a0195690a5b7898b2b6e143c4fca957290f
Andrew Noblet <andrewbnoblet@gmail.com>
2019-10-07 12:33:09 -0400

    Chore: Save

--- src/components/Blog/Component.ts
+++ src/components/Blog/Component.ts
@@ -1,14 +1,18 @@
-import { LitElement, customElement, property } from "lit-element";
+import { LitElement, css, customElement, property, unsafeCSS } from "lit-element";
 
 import { BeforeRender } from "@anoblet/mixins";
 import GlobalStyle from "../../GlobalStyle";
-import Style from "./Style";
 import Template from "./Template";
 import { getCollection } from "../../Firebase";
 
+const styleImport = require("./style.css");
+const style = css`
+  ${unsafeCSS(styleImport)}
+`;
+
 @customElement("blog-component")
 export class Blog extends BeforeRender(LitElement) {
-  public static styles = [GlobalStyle, Style];
+  public static styles = [GlobalStyle, style];
   public render = Template.bind(this);
 
   @property() public posts: any = [];
