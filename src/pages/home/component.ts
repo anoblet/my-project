5eada488f9db252182e5a7f8c7157628e180a837
Andrew Noblet <andrewbnoblet@gmail.com>
2019-10-07 10:27:12 -0400

    Chore: Save

--- src/pages/home/component.ts
+++ src/pages/home/component.ts
@@ -1,8 +1,12 @@
-import { LitElement, customElement } from "lit-element";
+import { LitElement, css, customElement, unsafeCSS } from "lit-element";
 
 import globalStyle from "../../GlobalStyle";
-import style from "./style";
 import template from "./template";
+
+const styleImport = require("./style.css");
+const style = css`
+  ${unsafeCSS(styleImport)}
+`;
 
 @customElement("page-home")
 export class PageHome extends LitElement {
