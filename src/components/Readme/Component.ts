c1a1bc1952037a411e68535eb51e24f657dd55fd
Andrew Noblet <andrewbnoblet@gmail.com>
2019-10-10 03:36:40 -0400

    Chore: Save

---
+++ src/components/Readme/Component.ts
@@ -0,0 +1,33 @@
+import "@anoblet/markdown-component";
+
+import { LitElement, css, customElement, html, unsafeCSS } from "lit-element";
+
+import GlobalStyle from "../../GlobalStyle";
+import { applyStyle } from "@anoblet/utility";
+
+const styleImport = require("./style.css");
+const style = css`
+  ${unsafeCSS(styleImport)}
+`;
+
+@customElement("readme-component")
+export class ReadmeComponent extends LitElement {
+  public firstUpdated() {
+    applyStyle(
+      this.shadowRoot.querySelector("markdown-component"),
+      GlobalStyle
+    );
+    applyStyle(
+      this.shadowRoot.querySelector("markdown-component"),
+      style
+    );
+  }
+
+  public render() {
+    return html`
+      <markdown-component
+        src="https://raw.githubusercontent.com/anoblet/my-project/master/README.md"
+      ></markdown-component>
+    `;
+  }
+}
