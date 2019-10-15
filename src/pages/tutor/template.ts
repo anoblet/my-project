a75ec5adcc45b4ffccce356115f9707a0fcd2ba6
Andrew Noblet <andrewbnoblet@gmail.com>
2019-10-15 15:50:19 -0400

    Chore: Save

--- src/pages/tutor/template.ts
+++ src/pages/tutor/template.ts
@@ -1,61 +1,35 @@
 import "@anoblet/grid-component";
-import "@anoblet/ratio-component";
-
-import { create, extension, notes, show_chart } from "@anoblet/material-icons";
+import "@anoblet/card-component";
 
 import { html } from "lit-element";
 
 export default function() {
   return html`
-    <grid-component id="grid">
-      <div class="grid-item">
-        <a href="/components">
-          <ratio-component>
-            <div class="item">
-              <span class="label"><h1>Components</h1></span>
-              <span class="icon">
-                ${extension}
-              </span>
-            </div>
-          </ratio-component>
-        </a>
-      </div>
-      <div class="grid-item">
-        <a href="/readme">
-          <ratio-component>
-            <div class="item">
-              <span class="label"><h1>Readme</h1></span>
-              <span class="icon">
-                ${notes}
-              </span>
-            </div>
-          </ratio-component>
-        </a>
-      </div>
-      <div class="grid-item">
-        <a href="/blog">
-          <ratio-component>
-            <div class="item">
-              <span class="label"><h1>Blog</h1></span>
-              <span class="icon">
-                ${create}
-              </span>
-            </div></ratio-component
-          ></a
-        >
-      </div>
-      <div class="grid-item">
-        <a href="/Performance">
-          <ratio-component>
-            <div class="item">
-              <span class="label"><h1>Performance</h1></span>
-              <span class="icon">
-                ${show_chart}
-              </span>
-            </div></ratio-component
-          ></a
-        >
-      </div>
-    </grid-component>
+    <grid-component
+      ><card-component title="Hi!"
+        ><h1 slot="title">Tutoring</h1>
+        <div slot="body">
+          <h3>I offer one on one tutoring that is tailored to your needs.</h3>
+          <ul>
+            <li>Topics</li>
+            <ul>
+              <li>Word</li>
+              <li>Excel</li>
+              <li>Powerpoint</li>
+              <li>Email</li>
+              <li>Web browsing</li>
+            </ul>
+            <li>
+              Qualities
+            </li>
+            <ul>
+              <li>Patient</li>
+              <li>Caring</li>
+              <li>Individualized</li>
+            </ul>
+          </ul>
+        </div></card-component
+      ></grid-component
+    >
   `;
 }
